6cd78076c820a3c4df2373da9348b7797e1dc462
Hannes Mildner <hannes.mildner@cern.ch>
2015-07-21 18:55:48 +0200

    plotphys14-spring15 btagging curves

---
+++ plot_roc_phys14_spring15.py
@@ -0,0 +1,33 @@
+from plotutils import *
+s4j3t="(N_Jets==4&&N_BTagsM==3)"
+s4j4t="(N_Jets==4&&N_BTagsM>=4)"
+s5j3t="(N_Jets==5&&N_BTagsM==3)"
+s5j4t="(N_Jets==5&&N_BTagsM>=4)"
+s6j2t="(N_Jets>=6&&N_BTagsM==2)"
+s6j3t="(N_Jets>=6&&N_BTagsM==3)"
+s6j4t="(N_Jets>=6&&N_BTagsM>=4)"
+
+
+samples=[Sample('t#bar{t} spring15 powheg',ROOT.kBlue,'/nfs/dust/cms/user/kelmorab/Spring15_Base16thJuly/forTraining/ttbar/ttbar_nominal.root','') ,
+         Sample('t#bar{t} spring15 amcNLO',ROOT.kRed,'/nfs/dust/cms/user/kelmorab/Spring15_Base16thJuly/forLimit/ttbar/ttbar_nominal.root',''), 
+         ]
+
+
+plots=[
+    Plot(ROOT.TH1F("Jet_CSVb" ,"CSVv2 IVF b-jets",40,0,1),"Jet_CSV","abs(Jet_Flav)>4.5&&abs(Jet_Flav)<5.5"),
+    Plot(ROOT.TH1F("Jet_CSVl" ,"CSVv2 IVF l-jets",40,0,1),"Jet_CSV","abs(Jet_Flav)<3.5||abs(Jet_Flav)>5.5"),
+]
+
+
+
+listOfhistoLists=createHistoLists_fromTree(plots,samples,'MVATree')
+
+writeListOfhistoLists(listOfhistoLists,samples,'btag_plots')
+
+roc_s15=getROC(listOfhistoLists[0][0],listOfhistoLists[1][0],False)
+roc_p14=getROC(listOfhistoLists[0][1],listOfhistoLists[1][1],False)
+writeListOfROCs([roc_s15,roc_p14],['spring15', 'phys14'],[ROOT.kBlue,ROOT.kRed],'btag_roc',True,False)
+
+eff_s15=getEff(listOfhistoLists[0][0])
+eff_p14=getEff(listOfhistoLists[0][1])
+writeListOfROCs([eff_s15,eff_p14],['spring15', 'phys14'],[ROOT.kBlue,ROOT.kRed],'btag_eff',False,True)
