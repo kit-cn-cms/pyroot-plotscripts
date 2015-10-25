1ee483ec92156f9b94b4c427ee553954d507e6ff
Hannes Mildner <hannes.mildner@cern.ch>
2015-10-25 21:39:58 +0100

    testscript

---
+++ code_generation/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+cd /afs/desy.de/user/h/hmildner/CMSSW_7_4_14/src
+eval `scram runtime -sh`
+cd -
+
+export FILENAMES="/nfs/dust/cms/user/hmildner/trees1019/ttbar_nominal.root"
+export OUTFILENAME="testplots.root"
+export MAXEVENTS="200000000"
+export PROCESSNAME="ttbarBBbar"
+./plot_syst
