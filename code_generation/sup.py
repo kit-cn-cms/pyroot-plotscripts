d974dad325b2df854fc5b13adb579675f667799a
Hannes Mildner <hannes.mildner@cern.ch>
2015-11-03 17:13:29 +0100

    updated to most recent commit script

--- code_generation/sup.py
+++ code_generation/sup.py
@@ -4,26 +4,25 @@
 import os
 import sys
 import datetime
+from subprocess import call
 pattern=''
 path=''
-if len(sys.argv) > 1:
-    path= sys.argv[1]+'/'
-if len(sys.argv) > 2:
-    pattern= sys.argv[2]
-from subprocess import call
+files=[]
+if len(sys.argv) > 1 and sys.argv[1]=='-f':
+    files = [os.path.join(root, name)
+             for root, dirs, files in os.walk('./'+path)
+             for name in files
+             if pattern in name and name.endswith((".sh"))]
+    if len(sys.argv) > 2:
+        path= sys.argv[2]+'/'
+    if len(sys.argv) > 3:
+        pattern= sys.argv[3]
+
+else:
+    files=sys.argv[1:]
 
 if not os.path.exists('logs'):
     os.makedirs('logs')
-logfile='logfile.txt'
-if os.path.exists(logfile):
-    logfile+=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-#f_list=open(logfile,'w')
 
-files = [os.path.join(root, name)
-                     for root, dirs, files in os.walk('./'+path)
-                     for name in files
-                     if pattern in name and name.endswith((".sh"))]
 for f in files:
     call(['qsub', '-cwd', '-S', '/bin/bash','-l', 'h=bird*', '-hard','-l', 'os=sld6', '-l' ,'h_vmem=2000M', '-l', 's_vmem=2000M' ,'-o', 'logs/$JOB_NAME.o$JOB_ID', '-e', 'logs/$JOB_NAME.e$JOB_ID', f])
-#    f_list.write(f+'\n')
-#f_list.close()
