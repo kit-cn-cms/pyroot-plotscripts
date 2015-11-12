cbe4517cf00c0d91da011fb0a2ba3673093560ee
Hannes Mildner <hannes.mildner@cern.ch>
2015-11-12 09:31:50 +0100

    expanded example script

--- plot_parallel.py
+++ plot_parallel.py
@@ -2,35 +2,43 @@ from scriptgenerator import *
 from plotutils import *
 import sys
 
-lumi='1.28'
-path='/nfs/dust/cms/user/hmildner/trees1027/'
+path='/nfs/dust/cms/user/hmildner/trees1107/'
+pathnew='/nfs/dust/cms/user/hmildner/trees1111/'
+name='makeallplots'
 
-sel_all="(N_TightLeptons==1)*(N_LooseLeptons==1)*(N_BTagsM>=2)*(N_Jets>=4)"
-sel_echan="(N_TightElectrons==1)*(N_LooseElectrons==1)*(N_BTagsM>=2)*(N_Jets>=4)"
-sel_muchan="(N_TightElectrons==1)*(N_LooseElectrons==1)*(N_BTagsM>=2)*(N_Jets>=4)"
-catnames=["","echan","muchan"]
-catsels=[sel_all,sel_echan,sel_muchan]
+sel_singleel="(N_LooseMuons==0)" # need to veto muon events in electron dataset to avoid double countung
+sel_singlemu="(N_LooseElectrons==0)" # and vice versa...
+mcweight='(1.28*Weight_PV)' # some weights are only applied on mc
+# selections for categories
+sel_all="((N_TightLeptons==1)*(N_LooseLeptons==1)*(N_BTagsM>=2)*(N_Jets>=4))" # l+jets channel
+sel_muchan="((N_TightMuons==1)*(N_LooseMuons==1)*(N_LooseElectrons==0)*(N_BTagsM>=2)*(N_Jets>=4))" # e+jets channel
+sel_echan="((N_TightElectrons==1)*(N_LooseElectrons==1)*(N_LooseMuons==0)*(N_BTagsM>=2)*(N_Jets>=4))" # mu+jets channel
+sel_zmu="((N_TightElectrons==2)*(N_LooseElectrons==2)*(N_LooseMuons==0)*(DiElectron_M>81)*(DiElectron_M<101))" # z-control region for mumu
+sel_zel="((N_TightMuons==2)*(N_LooseMuons==2)*(N_LooseElectrons==0)*(DiElectron_M>81)*(DiElectron_M<101))" # z-control region for elel
+sel_wmu="((N_TightMuons==1)*(N_LooseMuons==1)*(N_LooseElectrons==0)*(N_Jets>=0))" # W to mu
+sel_wel="((N_TightElectrons==1)*(N_LooseElectrons==1)*(N_LooseMuons==0)*(N_Jets>=0))" # W to el
+sel_pretagmu="((N_TightMuons==1)*(N_LooseMuons==1)*(N_LooseElectrons==0)*(N_Jets>=4))" # e+jets channel without btag
+sel_pretagel="((N_TightElectrons==1)*(N_LooseElectrons==1)*(N_LooseMuons==0)*(N_Jets>=4))" # mu+jets channel without btag
+catsels=[sel_all,sel_echan,sel_muchan,sel_zmu,sel_zel,sel_wmu,sel_wel,sel_pretagmu,sel_pretagel]
+catnames=["","_echan","_muchan","_ztomumu","_ztoelel","_wtomu","_wtoel","_pretagmu","_pretagel"] # names of categories
+# data samples (name, color, path to files, selection, nickname_without_special_characters,optional: number of events for cross check)
+samples_data=[Sample('SingleMu',ROOT.kBlack,path+'/mu_*/*nominal*.root',sel_singlemu,'SingleMu',8427020+6226674),
+              Sample('SingleEl',ROOT.kBlack,path+'/el_*/*nominal*.root',sel_singleel,'SingleEl',4692772+3638693)]
 
-sel_mu='(N_TightMuons==1)*(N_LooseMuons==1)*(N_LooseElectrons==0)'
-sel_el='(N_TightElectrons==1)*(N_LooseElectrons==1)*(N_LooseMuons==0)'
-mcmatch='(BoostedJet_Top_Pt[0]>0)&&((BoostedJet_Dr_GenB[0]>=0&&BoostedJet_Dr_GenB[0]<1.5)&&(BoostedJet_Dr_GenQ1[0]>=0&&BoostedJet_Dr_GenQ1[0]<1.5)&&(BoostedJet_Dr_GenQ2[0]>=0&&BoostedJet_Dr_GenQ2[0]<1.5))'
-notmcmatch='(!((BoostedJet_Top_Pt[0]>0)&&((BoostedJet_Dr_GenB[0]>=0&&BoostedJet_Dr_GenB[0]<1.5)&&(BoostedJet_Dr_GenQ1[0]>=0&&BoostedJet_Dr_GenQ1[0]<1.5)&&(BoostedJet_Dr_GenQ2[0]>=0&&BoostedJet_Dr_GenQ2[0]<1.5))))'
-# samples
-
-samples_data=[Sample('SingleMu',ROOT.kBlack,path+'/mu_prompt/*nominal*.root',sel_mu),
-              Sample('SingleMuRe',ROOT.kBlack,path+'/mu_reminiaod/*nominal*.root',sel_mu),
-              Sample('SingleEl',ROOT.kBlack,path+'/el_prompt/*nominal*.root',sel_el),
-              Sample('SingleElRe',ROOT.kBlack,path+'/el_reminiaod/*nominal*.root',sel_el)]
-samples=[Sample('t#bar{t}H',ROOT.kBlue+1,path+'/ttH*/*nominal*.root',lumi,'ttH') ,     
-         Sample('t#bar{t}',ROOT.kRed+1,path+'/ttbar/*nominal*.root',lumi,'ttbar') ,     
-         Sample('Single Top',ROOT.kMagenta,path+'/st_*/*nominal*.root',lumi,'SingleTop') , 
-         Sample('W+jets',ROOT.kGreen-3,path+'/Wjets/*nominal*.root',lumi,'Wjets') , 
-         Sample('Z+jets',ROOT.kAzure-2 ,path+'/Zjets_*/*nominal*root',lumi,'Zjets') , 
+# mc samples
+samples=[Sample('t#bar{t}H',ROOT.kBlue+1,pathnew+'/ttH*/*nominal*.root',mcweight,'ttH',1822862) ,     
+         Sample('t#bar{t}',ROOT.kRed+1,pathnew+'/ttbar/*nominal*.root',mcweight,'ttbar',24010004) ,     
+         Sample('Single Top',ROOT.kMagenta,pathnew+'/st*/*nominal*.root',mcweight,'SingleTop',2301345) , 
+         Sample('W+jets',ROOT.kGreen-3,pathnew+'/Wjets/*nominal*.root',mcweight,'Wjets',13599271) , 
+         Sample('Z+jets',ROOT.kAzure-2 ,pathnew+'/Zjets_*/*nominal*root',mcweight,'Zjets',10352375) , 
+         Sample('QCD',ROOT.kYellow ,path+'/QCD*/*nominal*root',mcweight,'QCD',9979) , 
 ]
 
+# book plots
 plots=[
     Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_Pt","p_{T} (leading CA 1.5 jet)",50,150,650),"BoostedJet_FatJet_Pt[0]","(BoostedJet_Top_Pt[0]>10)"),
     Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_Top_Pt","Filtered jet p_{T} (leading CA 1.5 jet)",50,0,500),"BoostedJet_Top_Pt[0]","(BoostedJet_Top_Pt[0]>10)"),
+    Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_Top_Eta","Filtered jet #eta (leading CA 1.5 jet)",50,-2.5,2.5),"BoostedJet_Top_Eta[0]","(BoostedJet_Top_Pt[0]>10)"),
     Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_NSub32","NSubjettiness ratio #tau_{3}/#tau_{2} (leading CA 1.5 jet)",50,0,1),"BoostedJet_Tau3Filtered[0]/BoostedJet_Tau2Filtered[0]","(BoostedJet_Top_Pt[0]>10)"),
     Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_NSub21","NSubjettiness ratio #tau_{2}/#tau_{1} (leading CA 1.5 jet)",50,0,1),"BoostedJet_Tau2Filtered[0]/BoostedJet_Tau1Filtered[0]","(BoostedJet_Top_Pt[0]>10)"),
     Plot(ROOT.TH1F("BoostedTop_TopHadCandidate_TopMVAOutput","BDT top tag (leading CA 1.5 jet)",50,-1,1),"BoostedJet_TopTag_BDT_Std[0]","(BoostedJet_Top_Pt[0]>10)"),
@@ -54,17 +62,39 @@ plots=[
     Plot(ROOT.TH1F("N_BoostedJets","Number of CA 1.5 jets",5,-0.5,4.5),"N_BoostedJets",''),
     Plot(ROOT.TH1F("ptalljets","p_{T} of all jets",60,0,300),"Jet_Pt",''),
     Plot(ROOT.TH1F("csvalljets","csv of all jets",44,-.1,1),"Jet_CSV",''),
-    Plot(ROOT.TH1F("csvallbjets","csv of all jets > 40 GeV",44,-.1,1),"Jet_CSV",'Jet_Pt>40'),
+    Plot(ROOT.TH1F("csvalljets40","csv of all jets > 40 GeV",44,-.1,1),"Jet_CSV",'Jet_Pt>40'),
+    Plot(ROOT.TH1F("leppt","lepton p_{T}",50,0,200),"LooseLepton_Pt[0]"),
+    Plot(ROOT.TH1F("lepeta","lepton #eta",50,-2.5,2.5),"LooseLepton_Eta[0]"),
+    Plot(ROOT.TH1F("MET","MET",50,0,200),"Evt_Pt_MET"),
+    Plot(ROOT.TH1F("METphi","MET #phi",64,-3.2,3.2),"Evt_Phi_MET"),
     Plot(ROOT.TH1F("N_PrimaryVertices","Reconstructed primary vertices",31,-.5,30.5),"N_PrimaryVertices",''),
     Plot(ROOT.TH1F("JT" ,"jet-tag categories",9,-0.5,8.5),"3*max(min(N_BTagsM-2,2),0)+max(min(N_Jets-4,2),0)","(N_BTagsM>=2&&N_Jets>=4)")
 
 ]
-outputpath=plotParallel("allplots",2000000,plots,samples+samples_data,catnames,catsels)
-listOfHistoLists=createHistoLists_fromSuperHistoFile(outputpath,samples,plots)
-listOfHistoListsData=createHistoLists_fromSuperHistoFile(outputpath,samples_data,plots)
+# plot parallel -- alternatively there are also options to plot more traditional that also return lists of histo lists
+outputpath=plotParallel(name,2000000,plots,samples+samples_data,catnames,catsels)
+listOfHistoLists=createHistoLists_fromSuperHistoFile(outputpath,samples,plots,1,catnames)
+listOfHistoListsData=createHistoLists_fromSuperHistoFile(outputpath,samples_data,plots,1,catnames)
+ntables=0
+# do some post processing
 for hld,hl in zip(listOfHistoListsData,listOfHistoLists):
     if "JT" in hld[0].GetName():
-        tablepath=("/".join((outputpath.split('/'))[:-1]))+"/yields"
-        print tablepath
+        for h in hld+hl:
+            h.GetXaxis().SetBinLabel(1,'4j2t')
+            h.GetXaxis().SetBinLabel(2,'5j2t')
+            h.GetXaxis().SetBinLabel(3,'6j2t')
+            h.GetXaxis().SetBinLabel(4,'4j3t')
+            h.GetXaxis().SetBinLabel(5,'5j3t')
+            h.GetXaxis().SetBinLabel(6,'6j3t')
+            h.GetXaxis().SetBinLabel(7,'4j4t')
+            h.GetXaxis().SetBinLabel(8,'5j4t')
+            h.GetXaxis().SetBinLabel(9,'6j4t')
+        ntables+=1
+        tablepath=("/".join((outputpath.split('/'))[:-1]))+"/"+name+"_yields"+str(ntables)
+        # make an event yield table
         eventYields(hld,hl,samples,tablepath)
-plotDataMC(listOfHistoListsData,listOfHistoLists,samples,outputpath,False,"")
+
+# plot dataMC comparison
+plotDataMC(listOfHistoListsData,listOfHistoLists,samples,name,False,"")
+# plot dataMC comparison with logscale
+#plotDataMC(listOfHistoListsData,listOfHistoLists,samples,name,True,"")
