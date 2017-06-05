61f1ef7364589b93624009a04e31ddfa1ddf3114
Karim <karim.morabit@kit.edu>
2017-06-05 14:15:26 +0200

    new DNN limit scripts

--- plot_LimitsJTDNNv10.py
+++ plot_LimitsJTDNNv10.py
@@ -20,7 +20,7 @@ doDrawParallel=True
 
 
 # output name
-name='limits_JTBDT_Spring17v10'
+name='limits_JTDNN_Spring17v10'
 
 # define categories
 categories_=[
@@ -34,19 +34,19 @@ categories_=[
               ("(N_Jets>=6&&N_BTagsM==3)","ljets_jge6_t3",""),
               ("(N_Jets>=6&&N_BTagsM>=4)","ljets_jge6_tge4","")
 ]
-
+# DANGERZONE
+# Need to remove the other node. Do not know what happens if the datacardmaker tries to use empty histograms
 categories=[]
-#categories=categories_
-bdtcuts=[-0.2,-0.2,0.2,0.26,0.17,0.26,0.1,0.12,0.13]
-#for cat,bdt in zip(categories_,bdtcuts):
-  #if cat[1] in ["ljets_jge6_tge4","ljets_j5_tge4","ljets_j4_t4","ljets_jge6_t3","ljets_j5_t3","ljets_j4_t3"]:
-    #categories.append(('('+cat[0]+')*(finalbdt_'+cat[1]+'>'+str(bdt)+')',cat[1]+'_high') )
-    #categories.append(('('+cat[0]+')*(finalbdt_'+cat[1]+'<='+str(bdt)+')',cat[1]+'_low') )
-  #else:
-    #categories.append(cat)
+
+nhistobins= [  20,20, 	10,   10,    10,    10,   20,   10,   10 ]
+minxvals=   [ 200, 200, 0.16,  0.16, 0.16,   0.16, 0.16, 0.16,   0.16]
+maxxvals=   [800,800,    0.45,  0.55,   0.55,    0.55,  0.45,  0.55,    0.6]
+print len(nhistobins)
+print len(minxvals)
+print len(maxxvals)
 
 # add unsplit categories
-for cat,bdt in zip(categories_,bdtcuts):
+for cat in categories_:
   categories.append(cat)
 
 print categories
@@ -60,24 +60,26 @@ bdtset="Spring17v1"
 additionalvariables=[
 			'finalbdt_ljets_j4_t2:=Evt_HT_Jets',
 			'finalbdt_ljets_j5_t2:=Evt_HT_Jets',
-                      'finalbdt_ljets_j4_t3:='+bdtweightpath+'/weights_Final_43_'+bdtset+'.xml',
-                      'finalbdt_ljets_j4_t4:='+bdtweightpath+'/weights_Final_44_'+bdtset+'.xml',
-                      'finalbdt_ljets_j5_t3:='+bdtweightpath+'/weights_Final_53_'+bdtset+'.xml',
-                      'finalbdt_ljets_j5_tge4:='+bdtweightpath+'/weights_Final_54_'+bdtset+'.xml',
-                      'finalbdt_ljets_jge6_t2:='+bdtweightpath+'/weights_Final_62_'+bdtset+'.xml',
-                      'finalbdt_ljets_jge6_t3:='+bdtweightpath+'/weights_Final_63_'+bdtset+'.xml',
-                      'finalbdt_ljets_jge6_tge4:='+bdtweightpath+'/weights_Final_64_'+bdtset+'.xml',
-                      #'finalbdt_ljets_boosted:='+bdtweightpath+'/weights_Final_DB_boosted_76xmem.xml',
+                      'dummybdt_ljets_j4_t3:='+bdtweightpath+'/weights_Final_43_'+bdtset+'.xml',
+                      'dummybdt_ljets_j4_t4:='+bdtweightpath+'/weights_Final_44_'+bdtset+'.xml',
+                      'dummybdt_ljets_j5_t3:='+bdtweightpath+'/weights_Final_53_'+bdtset+'.xml',
+                      'dummybdt_ljets_j5_tge4:='+bdtweightpath+'/weights_Final_54_'+bdtset+'.xml',
+                      'dummybdt_ljets_jge6_t2:='+bdtweightpath+'/weights_Final_62_'+bdtset+'.xml',
+                      'dummybdt_ljets_jge6_t3:='+bdtweightpath+'/weights_Final_63_'+bdtset+'.xml',
+                      'dummybdt_ljets_jge6_tge4:='+bdtweightpath+'/weights_Final_64_'+bdtset+'.xml',
+                      'finalbdt_ljets_j4_tge3:=((N_Jets==4&&N_BTagsM==3)*dummybdt_ljets_j4_t3 + (N_Jets==4&&N_BTagsM==4)*dummybdt_ljets_j4_t4)',
+                      'finalbdt_ljets_j5_tge3:=((N_Jets==5&&N_BTagsM==3)*dummybdt_ljets_j5_t3 + (N_Jets==5&&N_BTagsM>=4)*dummybdt_ljets_j5_tge4)',
+                      'finalbdt_ljets_jge6_tge3:=((N_Jets>=6&&N_BTagsM==3)*dummybdt_ljets_jge6_t3 + (N_Jets>=6&&N_BTagsM>=4)*dummybdt_ljets_jge6_tge4)',
+               
                       "Muon_Pt","Electron_Pt","Muon_Eta","Electron_Eta","Jet_Pt","Jet_Eta","Jet_CSV","Jet_Flav","N_Jets","Jet_Phi","Jet_E","Jet_M",
                       "Evt_Pt_PrimaryLepton","Evt_E_PrimaryLepton","Evt_M_PrimaryLepton","Evt_Phi_PrimaryLepton","Evt_Eta_PrimaryLepton",
                       "Weight_CSV","Weight_CSVLFup","Weight_CSVLFdown","Weight_CSVHFup","Weight_CSVHFdown","Weight_CSVHFStats1up","Weight_CSVHFStats1down","Weight_CSVLFStats1up","Weight_CSVLFStats1down","Weight_CSVHFStats2up","Weight_CSVHFStats2down","Weight_CSVLFStats2up","Weight_CSVLFStats2down","Weight_CSVCErr1up","Weight_CSVCErr1down","Weight_CSVCErr2up","Weight_CSVCErr2down",
 ]
 
-nhistobins= [  20,20, 	20,   10,    20,    10,   20,   20,   10 ]
-minxvals=   [ 200, 200, -0.75,  -0.8, -0.8,   -0.8, -0.8, -0.8,   -0.8]
-maxxvals=   [800,800,    0.8,  0.75,   0.8,    0.7,  0.8,  0.8,    0.7]
 
-discrs =    ['finalbdt_ljets_j4_t2','finalbdt_ljets_j5_t2','finalbdt_ljets_j4_t3', 'finalbdt_ljets_j4_t4', 'finalbdt_ljets_j5_t3', 'finalbdt_ljets_j5_tge4', 'finalbdt_ljets_jge6_t2', 'finalbdt_ljets_jge6_t3', 'finalbdt_ljets_jge6_tge4']
+discrs =    ['finalbdt_ljets_j4_t2','finalbdt_ljets_j5_t2',
+	     'aachen_Out_ttH','aachen_Out_ttH','aachen_Out_ttH','aachen_Out_ttH','aachen_Out_ttH','aachen_Out_ttH','aachen_Out_ttH',
+	     ]
 discrname='finaldiscr'
 assert(len(nhistobins)==len(maxxvals))
 assert(len(nhistobins)==len(minxvals))
@@ -87,6 +89,7 @@ assert(len(nhistobins)==len(discrs))
 # get input for plotting function
 bins= [c[0] for c in categories]
 binlabels= [c[1] for c in categories]
+samples=samplesLimits
 allsystnames=weightSystNames+otherSystNames+PSSystNames
 
 # samples
@@ -136,7 +139,7 @@ if not os.path.exists(name):
 # plot everthing
 
 if doDrawParallel==False or len(sys.argv) == 1 :                      #if some option is given plotParallelStep will be skipped
-    outputpath=plotParallel(name,5000000,plots+bdtsHighNBins,samples+samples_data+systsamples,[''],['1.'],weightSystNames, systWeights,additionalvariables,[],"/nfs/dust/cms/user/kelmorab/plotscriptsSpring17/pyroot-plotscripts/treejson28052017.json",otherSystNames+PSSystNames)
+    outputpath=plotParallel(name,5000000,plots+bdtsHighNBins,samples+samples_data+systsamples,[''],['1.'],weightSystNames, systWeights,additionalvariables,[],"/nfs/dust/cms/user/kelmorab/plotscriptsSpring17/pyroot-plotscripts/treejson28052017.json",otherSystNames+PSSystNames,True)
 else:
     workdir=os.getcwd()+'/workdir/'+name
     outputpath=workdir+'/output.root'
