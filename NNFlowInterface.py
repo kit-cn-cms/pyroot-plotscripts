131cfc43c2c674d1f3d1f20c527ed09be606d6be
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2017-08-10 14:12:41 +0200

    NNFlowInterface: First version of general NNFlowInterface class, bugs are still expected ttbb-nn-analysis: Changed to NNFlowInterface which in the future could be used for any TF NN gitignore: Added first version

--- NNFlowInterface.py
+++ NNFlowInterface.py
@@ -4,8 +4,6 @@
 # There are several methods which you will need to update for your own classifier 
 # Then use the interface by passing THIS file path to the plotparallel function
 
-import sys
-
 class theInterface:
   
   def __init__(self):
@@ -50,35 +48,35 @@ class theInterface:
     
     # Update / read input variable list, the assumption is that the file is located under self.modelFolderPath/model_properties/inputVariables.txt
     inputVariablesListLoc = self.modelFolderPath + '/model_properties/inputVariables.txt'
-    if(self.debugOutput)
+    if(self.debugOutput):
       print "Update function: Start reading input variables list file: ", inputVariablesListLoc
-    self.inputVariablesList = readListFromFile(inputVariablesListLoc)
+    self.inputVariablesList = self.readListFromFile(inputVariablesListLoc)
     
     # Update / read output labels list, the assumption is that the file is located under self.modelFolderPath/model_properties/outputLabels.txt
     outputLabelsListLoc = self.modelFolderPath + '/model_properties/outputLabels.txt'
-    if(self.debugOutput)
+    if(self.debugOutput):
       print "Update function: Start reading output labels list file: ", outputLabelsListLoc
-    self.outputLabelList = readListFromFile(outputLabelsListLoc)
+    self.outputLabelList = self.readListFromFile(outputLabelsListLoc)
     # Add tf_class variable to list
     self.outputLabelList.append('tf_class')
   
     # Update / read preselection list, the assumption is that the file is located under self.modelFolderPath/model_properties/preselection.txt
     preselectionListLoc = self.modelFolderPath + '/model_properties/preselection.txt'
-    if(self.debugOutput)
+    if(self.debugOutput):
       print "Update function: Start reading preselection list file: ", preselectionListLoc
-    self.preselectionList = readListFromFile(preselectionListLoc)
+    self.preselectionList = self.readListFromFile(preselectionListLoc)
   
     # Update / read unitTest input values list, the assumption is that the file is located under self.modelFolderPath/model_properties/unitTestInputValues.txt
     unitTestInputValuesListLoc = self.modelFolderPath + '/model_properties/unitTestInputValues.txt'
-    if(self.debugOutput)
+    if(self.debugOutput):
       print "Update function: Start reading unitTest input values list file: ", unitTestInputValuesListLoc
-    self.unitTestInputValuesList = readListFromFile(unitTestInputValuesListLoc)
+    self.unitTestInputValuesList = self.readListFromFile(unitTestInputValuesListLoc)
   
     # Update / read unitTest output values list, the assumption is that the file is located under self.modelFolderPath/model_properties/unitTestOutputValues.txt
     unitTestOutputValuesListLoc = self.modelFolderPath + '/model_properties/unitTestOutputValues.txt'
-    if(self.debugOutput)
+    if(self.debugOutput):
       print "Update function: Start reading unitTest output values list file: ", unitTestOutputValuesListLoc
-    self.unitTestOutputValuesList = readListFromFile(unitTestOutputValuesListLoc)
+    self.unitTestOutputValuesList = self.readListFromFile(unitTestOutputValuesListLoc)
   
   ## Setter functions
   
@@ -117,7 +115,7 @@ class theInterface:
         
     except IOError:
       print "Could not read file:", fileLocation
-      sys.exit()
+      raise
   
     for singleContent in fileContent:
       # Make sure that lines does not only consists of whitespace or line break characters
@@ -131,6 +129,16 @@ class theInterface:
     return tempList
   
 
+  ## Getter function
+  def getAdditionalVariablesList(self):
+    """Function returns list with all input variables which have to be defined in main analysis script as additional variables
+    Returns: String list 
+    """
+  
+    if(not self.updateFunctionCalled):
+      self.update()
+      
+    return self.inputVariablesList
   
   
   ## Interface functions
@@ -140,7 +148,7 @@ class theInterface:
     You also need to define them in the getVariableInitLines method
     """
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     return self.outputLabelsList
@@ -151,7 +159,7 @@ class theInterface:
     DNN header is relative to CMSSW BASE
     """
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     retstr="""
@@ -170,7 +178,7 @@ class theInterface:
   def getAdditionalFunctionDefinitionLines(self):
     """ Here you define any additional functions you will need"""
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     # NNFlowInterface does not need any additional functions :-)
@@ -181,7 +189,7 @@ class theInterface:
   def getBeforeLoopLines(self):
     """ Here you write the code which shgould be inserted before the main event loop"""
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     rstr='''
@@ -214,14 +222,14 @@ class theInterface:
   def getVariableInitInsideEventLoopLines(self):
     """ Initialize variables INSIDE event loop"""
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     rstr='''
     // output node variables\n
     '''
     
-    for(outputLabel in outputLabelList):
+    for outputLabel in outputLabelList:
       # Handle tf_class variable which is an integer
       if(outputLabel == 'tf_class'):
         rstr += '''int tf_class = -2;\n'''
@@ -231,7 +239,7 @@ class theInterface:
     # Add preselection string to speed up plotting
     if(len(self.preselectionList) > 0):
       rstr += '''\nNNFlowInterface preselection\n'''
-      for(preselection in preselectionList):
+      for preselection in preselectionList:
         rstr += '''\nif(!''' + preselection + ''') continue;\n'''
  
     return rstr
@@ -242,7 +250,7 @@ class theInterface:
     This is also inside of the event loop
     """
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     rstr='''
@@ -251,10 +259,10 @@ class theInterface:
     std::vector<std::string> inputNames = {
     '''
   
-    for(inputVariable in self.inputVariablesList):
+    for inputVariable in self.inputVariablesList:
       rstr += '''"''' + inputVariable + '''",\n'''
   
-    rstr= '''
+    rstr += '''
     };
 
     // Construct float vector containing input values of a single event
@@ -271,28 +279,44 @@ class theInterface:
       }
     }  
   
-  // Evaluate the output for an event
-  std::vector<float> outputValuesReturnVec;
-  outputValuesReturnVec = modelUser.evalModel(inputValues);
-  tf_ttlight=outputValuesReturnVec[0];
-  tf_ttcc=outputValuesReturnVec[1];
-  tf_ttb=outputValuesReturnVec[2];
-  tf_tt2b=outputValuesReturnVec[3];
-  tf_ttbb=outputValuesReturnVec[4];
-  tf_ttH=outputValuesReturnVec[5];
+    // Evaluate the output for an event
+    std::vector<float> outputValuesReturnVec;
+    outputValuesReturnVec = modelUser.evalModel(inputValues);
+    
+    '''
+    
+    # Add tf output label variables handling and handling of tf_class
+    for outputLabelNumber, outputLabel in enumerate(self.outputLabelsList):
+      if(outputLabel == 'tf_class'):
+        rstr += '''
+        // Check which class has largest outputValue and set tf_class variable accordingly
+        int currentIndexOfOutputValuesReturnVec = 0;
+        float tempLargestOutputValue = -2.0;
+        for (auto outputValue: outputValuesReturnVec) {
+          currentIndexOfOutputValuesReturnVec++;
+          if(outputValue > tempLargestOutputValue) {
+            tempLargestOutputValue = outputValue;
+            tf_class = currentIndexOfOutputValuesReturnVec;
+          }
+        }
+        
+        '''
+      else:
+        rstr += outputLabel + ''' =  outputValuesReturnVec[''' + outputLabelNumber + '''];\n'''
   
-  bool printstuff=1;
-  if(printstuff){
-    std::cout << "-----NNFlowInterface-----" << std::endl;
-    std::cout <<"tf ttlight node " << tf_ttlight << std::endl;
-    std::cout <<"tf ttcc node " << tf_ttcc << std::endl;
-    std::cout <<"tf ttb node " << tf_ttb << std::endl;
-    std::cout <<"tf tt2b node " << tf_tt2b << std::endl;
-    std::cout <<"tf ttbb node " << tf_ttbb << std::endl;
-    std::cout <<"tf ttH node " << tf_ttH << std::endl;
-    }
-  
-  '''
+       
+    if(self.debugOutput):
+      rstr += '''
+      std::cout << "-----NNFlowInterface debug output-----" << std::endl;
+      std::cout <<"Input values " << std::endl;
+      for (auto inputValue: inputValues) {
+        std::cout << inputValue << ", " ;
+      }
+      
+      '''
+      
+      for outputLabel in self.outputLabelsList:
+        rstr += '''std::cout << "''' + outputLabel +  '''" << ''' + outputLabel + '''<< std::endl;\n'''
 
     return rstr
 
@@ -300,89 +324,45 @@ class theInterface:
   def getTestCallLines(self):
     """ Here is code calling a test function to see wether the calling works as it should"""
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
     
-    rstr="""
+    rstr = '''
+    // NNFlowInterface unit test
+    // Vector containing input variables values
     std::vector<float> eventVec = {
-  75.3191833496, 449.89855957, 0.0750138610601, 3.22019815445,
-  0.0, 217.826843262, -0.700150609016, -0.794458508492,
-  0.0, 3.18069839478, 2.90678453445, 0.502826452255,
-  1.75928318501, 0.99976426363, 0.398145616055, -0.0298533830792,
-  0.16509616375, 0.0580038204789, 866.979003906, 0.878696382046,
-  0.401795059443, 0.406872868538, 217.826843262, 0.489256113768,
-  0.895407497883, 0.113197058439, 0.000279261381365, 0.0824939981103,
-  0.878696382046, 1.96167123318, 3.18069839478, 2.04820561409,
-  2.80315327644, 0.976631700993, 0.99976426363, 2.27863311768,
-  3.22019815445, 1.35024535656, 3.0377805233, 75.1506958008,
-  0.234085097909, -1.10028648376, 0.216148853302, 0.243053168058,
-  449.898529053, 269.498138428, 0.40179502964, 3.28364706039,
-  143.182815552, 217.826843262, 143.743743896, 307.974884033,
-  95.3141174316, 3.02768826485, 217.826843262, 36.9866218567,
-  43.5902404785, 75.3191833496, 217.826843262, 64.619102478,
-  -0.0148671893403, 8.54773235321, 217.826843262, 866.979003906,
-  7.07996463776, -0.0760409533978, 0.80216896534, 135.370361328,
-  64.2974319458, 91.3021011353, 85.4920501709, 45.0300445557,
-  2.66747665405, 4.55489873886, 0.419706910849, -0.323976635933,
-  0.0, 2.99721050262, 3.21775770187, 2.97493648529,
-  2.97493648529, 0.69654083252, 2.94123387337, 3.3925049305,
-  2.86213493347, 2.86213493347, 0.976631700993, 2.66130447388,
-  3.2393848896, 2.59573721886, 2.59573721886, 0.976631700993,
-  2.66130447388, 3.2393848896, 2.59573721886, 2.59573721886,
-  0.976631700993, 2.66130447388, 3.2393848896, 2.59573721886,
-  2.59573721886, 1.04637384415, 1.68253695965, 3.85681200027,
-  0.730289399624, 0.283886820078, 3.87645053864, 3.57489085197,
-  2.94803261757, 162.348922729, 204.765716553, 0.506848037243,
-  0.517021179199, 0.176898449659, 0.0618144534528, 0.542471766472,
-  0.437094211578, 0.221899345517, 0.0443901047111, 0.0520681291819,
-  0.212498500943, 0.336108744144, 0.110937654972, 0.535665273666,
-  0.42041388154, 0.570250093937, 0.413498193026, 0.0507325269282,
-  0.201323732734, 0.701995849609, 0.654439508915, 0.306296408176,
-  0.292550832033, 0.460287541151, 9.75731018116e-05, 1.01019764998e-07,
-  0.228382438421, 0.194773316383, 4.30822183262e-05, 4.17746157666e-08,
-  4.12119316451e-09, 6.29278495978e-09, 7.43852313079e-10, 3.84066167758e-10,
-  8.89584867991e-08, 1.81083024131e-08, 4.19009715813e-10, 1.42976547268e-10,
-  3.15395573125e-06, 3.02811440633e-06, 1.18623836443e-06, 6.69018390909e-07,
-  0.00130667432677, 0.00207811989821, 0.00062706816243, 0.000574074161705,
-  0.0282053686678, 0.00598005903885, 0.00035322556505, 0.000213710940443,
-  0.00150740402751, 0.00150740402751, 0.000832078629173, 0.000404736376368,
-  4.88632379003e-09, 4.88632379003e-09, 2.12132381106e-10, 1.78406872609e-11,
-  3.24154871123e-06, 3.24154871123e-06, 2.54942705169e-07, 4.40797727208e-08,
-  3.0, 2.0, 0.0, 6.0,
-  1.0, 7.0, 0.0, 18.0,
-  1.0, 0.0, 1.0, 3.0,
-  75.1506958008, -1.10028648376, -0.0148671893403, 0.80216896534,
-  45.0300445557, 0.91211861372, 0.878696382046, 0.637541413307,
-  0.275193244219, 0.149492889643, 0.0824939981103, 0.0824939981103,
-  0.91211861372, 0.149492889643, 0.878696382046, 0.637541413307,
-  0.275193244219, -0.147270709276, 0.490214288235, 0.44051322341,
-  0.148862197995, -0.862654268742, 0.269968181849, 164.798751831,
-  179.662216187, 37.6692733765, 77.646736145, 89.7762527466,
-  121.182106018, 1.49528670311, 1.80649805069, -0.105236373842,
-  -1.37420034409, 1.57253301144, -1.99037063122, 9.01439476013,
-  8.91282653809, 5.50948381424, 8.18263816833, 6.94388914108,
-  6.85209083557, -2.54293274879, 2.56145215034, 0.705195367336,
-  3.0642786026, -1.19489884377, -2.46118402481, 70.2493286133,
-  57.3906860352, 37.0587921143, 36.725402832, 35.6151771545,
-  32.4587554932, 0.995814323425, 0.977832615376, 0.968445897102,
-  0.804459810257, -0.163851693273, 0.965455949306
-  };
-
-    std::vector<float> outputValuesVec = { 0.26361305, 0.25014877, 0.24499501, 0.11612786, 0.10633284, 0.01878254 };
+    '''
     
-    // evaluate event for given model
+    for inputValue in self.unitTestInputValuesList:
+      rstr += ''' ''' + inputValue + ''',\n'''
+    
+    rstr += '''
+    };
+
+    // Vector containing output labels values
+    std::vector<float> outputValuesVec = {
+    '''
+    
+    for outputValue in self.unitTestOutputValuesList:
+      rstr += ''' ''' + outputValue + ''',\n'''
+    
+    rstr += '''
+    };
+    
+    // Evaluate unit test event for given model
     std::vector<float> outputValuesReturnVec = modelUser.evalModel(eventVec);
 
     // compare vectors for unit test
-    std::cout << "Doing NNFlowInterface unit test" << std::endl;
+    std::cout << "Compare NNFlowInterface unit test result with known output" << std::endl;
     std::cout << "No error printout means unit test succeeded." << std::endl;
     for (unsigned int i = 0; i < outputValuesReturnVec.size(); i++)
     {
-        std::cout << "Output Value: " << outputValuesReturnVec[i] << std::endl;
+        std::cout << "Calculated output Value: " << outputValuesReturnVec[i] << ", known output value: " << outputValuesVec[i]  << std::endl;
         assert(fabs(outputValuesReturnVec[i] - outputValuesVec[i]) < 0.00001 && "The unit test for the NNFlowInterface did not succeed.");
     }
-"""
+    '''
+    
     return rstr
   
   
@@ -392,11 +372,12 @@ class theInterface:
     Will be run after event loop
     """
     
-    if(!self.updateFunctionCalled)
+    if(not self.updateFunctionCalled):
       self.update()
     
-    rstr="""
-   """
+    rstr='''
+    '''
+    
     return rstr
   
   
