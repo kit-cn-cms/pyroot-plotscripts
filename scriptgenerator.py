ce9d4ec04acd62f5a757d6daf9c45911ffc1036b
Michael Wassmer <michael.wassmer@cern.ch>
2017-09-27 16:09:02 +0200

    added QCDHelper to scriptgenerator

--- scriptgenerator.py
+++ scriptgenerator.py
@@ -36,6 +36,8 @@ def getHead(dataBases,addCodeInterfaces=[]):
 #include <algorithm>
 #include <map>
 #include "TStopwatch.h"
+#include <TString.h>
+#include <TH2D.h>
 """
   for addCodeInt in addCodeInterfaces:
     retstr+=addCodeInt.getIncludeLines()
@@ -1045,6 +1047,151 @@ CSVHelper::getCSVWeight(const std::vector<double>& jetPts,
   return csvWgtTotal;
 }
 
+// QCD Helper to retrieve scale factor for QCD Estimation with iso inverted ntuples
+
+class QCDHelper
+{
+	public:
+		// see the definition/implementation file for a description of the functions 
+		QCDHelper(TString path_to_sf_file_);
+		~QCDHelper();
+		double GetScaleFactor(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons);
+		double GetScaleFactorError(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons);
+		double GetScaleFactorErrorUp(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons);
+		double GetScaleFactorErrorDown(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons);
+		void Reset();
+		void LoadFile(TString path_to_sf_file_);
+			
+	private:
+		// pointer for the root file containing the desired histograms
+		TFile* scalefactor_file = 0;
+		// path to the root file
+		TString path_to_sf_file = "";
+		// histograms containing the scale factors for electron and muon channel separately
+		TH2D* Mu_SF = 0;
+		TH2D* El_SF = 0;
+		// flag if the file and the histograms were read properly
+		bool initialized = false;
+};
+
+QCDHelper::QCDHelper(TString path_to_sf_file_)
+{
+	// just the constructor which loads a root file containing the histograms with the scale factors using the LoadFile function
+	LoadFile(path_to_sf_file_);
+}
+
+void QCDHelper::LoadFile(TString path_to_sf_file_)
+{
+	// this function loads the file given by a string and initializes the 2 needed histograms if the file was loaded correctly
+	path_to_sf_file = path_to_sf_file_;
+	if(path_to_sf_file!="")
+	{
+		scalefactor_file = TFile::Open(path_to_sf_file);
+	}
+	if(scalefactor_file)
+	{
+		El_SF = (TH2D*)scalefactor_file->Get("El_FakeSF");
+		Mu_SF = (TH2D*)scalefactor_file->Get("Mu_FakeSF");
+		initialized = true;
+	}
+}
+
+void QCDHelper::Reset()
+{
+	// resets all member data
+	scalefactor_file = 0;
+	path_to_sf_file = "";
+	Mu_SF = 0;
+	El_SF = 0;
+	initialized = false;
+}
+
+double QCDHelper::GetScaleFactor(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons)
+{
+	// this function gets the scale factor for a event dependent on the number of jets,btags and the lepton flavor
+	if(!initialized) return 0.;
+	int bin = -1;
+	double sf = 0.;
+	int N_jets = n_jets>6 ? 6 : n_jets;
+	int N_btags = n_btags>4 ? 4 : n_btags;
+	if(n_isoinverted_electrons==1&&n_isoinverted_muons==0) 
+	{
+		bin = El_SF->GetBin(N_jets,N_btags);
+		sf = El_SF->GetBinContent(bin);
+		// the values in the file are not correct for electron 6j4b category and have to be set manually
+		if(N_jets>=6&&N_btags>=4)
+		{
+			sf = 1.8;
+		}
+	}
+	else if(n_isoinverted_electrons==0&&n_isoinverted_muons==1)
+	{
+		bin = Mu_SF->GetBin(N_jets,N_btags);
+		sf = Mu_SF->GetBinContent(bin);
+	}
+	else 
+	{
+		return 0.;
+	}
+	return sf<0. ? 0. : sf;
+}
+
+double QCDHelper::GetScaleFactorError(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons)
+{
+	// this function gets the error on the scale factor for a event dependent on the number of jets,btags and the lepton flavor
+	if(!initialized) return 0.;
+	int bin = -1;
+	double sf_err = 0.;
+	int N_jets = n_jets>6 ? 6 : n_jets;
+	int N_btags = n_btags>4 ? 4 : n_btags;
+	if(n_isoinverted_electrons==1&&n_isoinverted_muons==0) 
+	{
+		bin = El_SF->GetBin(N_jets,N_btags);
+		sf_err = El_SF->GetBinError(bin);
+		// the values in the file are not correct for electron 6j4b category and have to be set manually
+		if(N_jets>=6&&N_btags>=4)
+		{
+			sf_err = 2.0;
+		}
+	}
+	else if(n_isoinverted_electrons==0&&n_isoinverted_muons==1)
+	{
+		bin = Mu_SF->GetBin(N_jets,N_btags);
+		sf_err = Mu_SF->GetBinError(bin);
+	}
+	else 
+	{
+		return 0.;
+	}
+	return sf_err<0. ? 0. : sf_err;
+}
+
+double QCDHelper::GetScaleFactorErrorUp(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons)
+{
+	// gets the scale factor + error
+	if(!initialized) return 0.;
+	double sf = GetScaleFactor(n_jets,n_btags,n_isoinverted_electrons,n_isoinverted_muons);
+	double sf_err = GetScaleFactorError(n_jets,n_btags,n_isoinverted_electrons,n_isoinverted_muons);
+	return sf+sf_err <0. ? 0. : sf+sf_err;
+}
+
+double QCDHelper::GetScaleFactorErrorDown(int n_jets, int n_btags, int n_isoinverted_electrons, int n_isoinverted_muons)
+{
+	// gets the scale factor - error
+	if(!initialized) return 0.;
+	double sf = GetScaleFactor(n_jets,n_btags,n_isoinverted_electrons,n_isoinverted_muons);
+	double sf_err = GetScaleFactorError(n_jets,n_btags,n_isoinverted_electrons,n_isoinverted_muons);
+	return sf-sf_err <0. ? 0. : sf-sf_err;
+}
+
+QCDHelper::~QCDHelper()
+{
+	// destructor which closes the file if it was loaded in the first place
+	if(initialized) scalefactor_file->Close();
+}
+
+
+
 // Helper struct to fill plots more efficiently
 // Until GCC 4.9 struct cannot have init values if one wants to initialize it with bracket lists
 struct structHelpFillHisto{
@@ -1093,9 +1240,11 @@ void plot(){
 
   std::string csvHFfile="/nfs/dust/cms/user/kelmorab/DataFilesForScriptGenerator/factorized_jes/csv_rwt_fit_hf_v2_final_2017_6_7_all.root";
   std::string csvLFfile="/nfs/dust/cms/user/kelmorab/DataFilesForScriptGenerator/factorized_jes/csv_rwt_fit_lf_v2_final_2017_6_7_all.root";
+  TString qcd_file = "/nfs/dust/cms/user/mwassmer/QCD_Estimation_September17/QCD_Estimation/QCD_Estimation_FakeScaleFactor_nominal.root";
   
   CSVHelper* internalCSVHelper= new CSVHelper(csvHFfile,csvLFfile, 5,4,3,v_SystTypes);
   LeptonSFHelper* internalLeptonSFHelper= new LeptonSFHelper();
+  QCDHelper* internalQCDHelper = new QCDHelper(qcd_file);
 
   // open files
   TChain* chain = new TChain("MVATree");
@@ -1553,6 +1702,10 @@ def startLoop():
   float internalCSVweight_CSVCErr2Up=1.0;
   float internalCSVweight_CSVCErr2Down=1.0;  
   
+  float internalQCDweight = 0.0;
+  float internalQCDweightup = 0.0;
+  float internalQCDweightdown = 0.0;
+  
   double tmpcsvWgtHF, tmpcsvWgtLF, tmpcsvWgtCF;
   
   internalCSVweight=internalCSVHelper->getCSVWeight(jetPts,jetEtas,jetCSVs,jetFlavors,internalSystType,tmpcsvWgtHF, tmpcsvWgtLF, tmpcsvWgtCF);
@@ -1576,7 +1729,9 @@ def startLoop():
   internalCSVweight_CSVCErr2Up=internalCSVHelper->getCSVWeight(jetPts,jetEtas,jetCSVs,jetFlavors,Systematics::CSVCErr2up,tmpcsvWgtHF, tmpcsvWgtLF, tmpcsvWgtCF)/internalCSVweight;
   internalCSVweight_CSVCErr2Down=internalCSVHelper->getCSVWeight(jetPts,jetEtas,jetCSVs,jetFlavors,Systematics::CSVCErr2down,tmpcsvWgtHF, tmpcsvWgtLF, tmpcsvWgtCF)/internalCSVweight;
   
- 
+  internalQCDweight=internalQCDHelper->GetScaleFactor(N_Jets,N_BTagsM,N_TightElectrons,N_TightMuons);
+  internalQCDweightup=internalQCDHelper->GetScaleFactorErrorUp(N_Jets,N_BTagsM,N_TightElectrons,N_TightMuons);
+  internalQCDweightdown=internalQCDHelper->GetScaleFactorErrorDown(N_Jets,N_BTagsM,N_TightElectrons,N_TightMuons);
  
  
 """
@@ -1744,6 +1899,7 @@ def createProgram(scriptname,plots,samples,catnames=[""],catselections=["1"],sys
 	    "internalMuIDWeight","internalMuIDWeightUp","internalMuIDWeightDown",
 	    "internalMuIsoWeight","internalMuIsoWeightUp","internalMuIsoWeightDown",
 	    "internalMuHIPWeight","internalMuHIPWeightUp","internalMuHIPWeightDown",
+	    "internalQCDweight","internalQCDweightup","internalQCDweightdown"
 ]
   for addCodeInt in addCodeInterfaces:
     vetolist+=addCodeInt.getExternalyCallableVariables()
