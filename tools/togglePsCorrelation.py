480b8d14d08b8c36826fa553727b9b8db9af8a09
Jan van der Linden <jan.linden@student.kit.edu>
2018-05-27 18:24:26 +0200

    removed togglePsCorrelation.py due to redundancy - changes marked with DISCLAIMER

--- tools/togglePsCorrelation.py
+++ tools/togglePsCorrelation.py
@@ -9,17 +9,40 @@ for f in files:
   inlist=list(theFile)
   theFile.close()
   outlist=[]
+  #DISCLAIMER the following lines were in the other file ---->
+  '''
+  anyToggled=False
+  for line in inlist:
+    for unc in listOfUncertaintiesToToggle:
+      if unc in line:
+        if "#" in line:
+          anyToggled=True
+  '''
+  #DISCLAIMER <----
   for line in inlist:
     newline=line
     if "number of nuisance parameters" in line:
       newline="kmax * #number of nuisance parameters\n"
+    #DISCLAIMER the following line was not in the other file
     lineContainsUncToRemove=False
+
     for unc in listOfUncertaintiesToToggle:
       if unc in line:
         if "#" in line:
           newline=line.replace("#","")
         else:
+          #DISCLAIMER the following lines were different in the other file ---->
           newline="#"+line 
+          '''
+          #anyToggled
+          if anyToggled==False:
+            if not "ttbar" in line:
+              newline="#"+line 
+          else:
+            #print line
+            newline="#"+line
+          '''
+          #DISCLAIMER <----
     outlist.append(newline)
   theFile=open(f,"w")
   for line in outlist:
