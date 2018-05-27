00a5ae8d61b7a602762a0c541a3f4399dd839809
Jan van der Linden <jan.linden@student.kit.edu>
2018-05-27 22:28:28 +0200

    moving some files to temporary folders

--- temp_tools/getMedians.py
+++ temp_tools/getMedians.py
@@ -1,11 +1,22 @@
+#OPEN: what does this script do?
 import ROOT
 import sys
 from array import array 
 
+#DISCLAIMER merging files getMedians and getMediansTTH
+#           use 'TTH' as last argument to execute whatever the old getMediansTTH.py did
+if sys.argv[-1] == "TTH":
+    doTTH = True
+else:
+    doTTU = False
+
 infname=sys.argv[1]
 
 cats=["ljets_j4_t2","ljets_j5_t2","ljets_j4_t3","ljets_j4_t4","ljets_j5_t3","ljets_j5_tge4","ljets_jge6_t2","ljets_jge6_t3","ljets_jge6_tge4",]
-bprocs=["ttH",]#"ttbarPlusCCbar","ttbarPlusB","ttbarPlus2B","ttbarPlusBBbar",]
+if doTTH:
+    bprocs=["ttH",]
+else:
+    bprocs=["ttbarOther","ttbarPlusCCbar","ttbarPlusB","ttbarPlus2B","ttbarPlusBBbar",]
 
 inf=ROOT.TFile(infname,"READ")
 
@@ -17,8 +28,9 @@ for c in cats:
     bkgs.append(inf.Get(b+"_finaldiscr_"+c))
     print bkgs[-1]
   stack=bkgs[0].Clone()
-  #for b in bkgs[1:]:
-    #stack.Add(b)
+  if not doTTH:
+    for b in bkgs[1:]:
+      stack.Add(b)
   probs=array("d",[0.5])
   quants=array("d",[0.0])
   stack.GetQuantiles(1,quants,probs)
