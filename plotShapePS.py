34449bb230d116936d3b3a09a1ac2eb5f547b11a
Karim <karim.morabit@kit.edu>
2018-12-06 15:59:24 +0100

    catch some case

--- plotShapePS.py
+++ plotShapePS.py
@@ -11,12 +11,14 @@ print "init"
 
 #acats=["ljets_j4_t4","ljets_j5_tge4","ljets_jge6_t3","ljets_jge6_tge4","ljets_j5_t3","ljets_j4_t3","ljets_jge6_t2"]
 
-acats=["ljets_jge6_t3","ljets_jge6_tge4","ljets_j4_t4","ljets_j5_tge4","ljets_j5_t3","ljets_j4_t3"]
+scats=["ljets_jge6_tge3","ljets_jge6_tge3","ljets_j4_tge3"]
 #acats+=["ljets_jge6_tge4_low","ljets_jge6_tge4_high","ljets_jge6_tge4_MEMONLY","ljets_jge6_t3_low","ljets_jge6_t3_high","ljets_jge6_t3_MEMONLY","ljets_jge6_tge3_tt2bnode","ljets_jge6_tge3_ttbbnode","ljets_jge6_tge3_ttbnode","ljets_jge6_tge3_ttccnode","ljets_jge6_tge3_ttlfnode","ljets_jge6_tge3_ttHnode"]
-acats+=["ljets_jge6_tge4_low","ljets_jge6_tge4_high","ljets_jge6_t3_low","ljets_jge6_t3_high","ljets_j4_t4_high","ljets_j5_tge4_high","ljets_j4_t4_high","ljets_j5_tge4_high"]
-#acats=["ljets_j5_t3","ljets_j4_t3"]
-#acats=["ljets_j5_t3"]
-
+#acats+=["ljets_jge6_tge4_low","ljets_jge6_tge4_high","ljets_jge6_t3_low","ljets_jge6_t3_high","ljets_j4_t4_high","ljets_j5_tge4_high","ljets_j4_t4_high","ljets_j5_tge4_high"]
+acats=[]
+for sc in scats:
+    for node in ["ttlfnode","ttbbnode","ttHnode","ttccnode","ttbnode","tt2bnode"]:
+      acats.append(sc+"_"+node)
+      
 doKStest=True
 
 cats=[]
@@ -25,14 +27,28 @@ for c in acats:
   #cats.append(c+"_low")
   cats.append(c)
 
+procs="ttH ttbarOther ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar ttbarIncl".split(" ")
+print procs
+
 print "ok"
 #systs=['CMS_ttHbb_FSR', 'CMS_ttHbb_ISR', 'CMS_ttHbb_HDAMP', 'CMS_ttHbb_UE','CMS_ttHbbFROMTREES_FSR', 'CMS_ttHbbFROMTREES_ISR', 'CMS_ttHbbFROMTREES_HDAMP', 'CMS_ttHbbFROMTREES_UE']
 #systs=['CMS_ttHbb_PDF', 'CMS_ttHbb_scaleMuF', 'CMS_ttHbb_scaleMuR']
-systs=['CMS_ttHbb_PU','CMS_ttHbb_scaleMuF', 'CMS_ttHbb_scaleMuR','CMS_scaleFlavorQCD_j']
+systs=['CMS_ttHbb_PU','CMS_ttHbb_scaleMuF', 'CMS_ttHbb_scaleMuR']
+asysts=['CMS_ttHbb_ttbar_FSR', 'CMS_ttHbb_ttbar_ISR', 'CMS_ttHbb_HDAMP', 'CMS_ttHbb_UE', 'CMS_ttHbb_PDF_ttbar', 'CMS_ttHbb_PDF', 'CMS_ttHbb_scaleMuF', 'CMS_ttHbb_scaleMuR', 'CMS_ttHbb_scaleMuF_ttbar', 'CMS_ttHbb_scaleMuR_ttbar', 'CMS_ttHbb_FSR', 'CMS_ttHbb_ISR', 'CMS_ttHbb_FSR_ttbar', 'CMS_ttHbb_ISR_ttbar', 'CMS_ttHbb_FSR_ttbarOther', 'CMS_ttHbb_FSR_ttbarPlusB', 'CMS_ttHbb_FSR_ttbarPlus2B', 'CMS_ttHbb_FSR_ttbarPlusBBbar', 'CMS_ttHbb_FSR_ttbarPlusCCbar', 'CMS_ttHbb_ISR_ttbarOther', 'CMS_ttHbb_ISR_ttbarPlusB', 'CMS_ttHbb_ISR_ttbarPlus2B', 'CMS_ttHbb_ISR_ttbarPlusBBbar', 'CMS_ttHbb_ISR_ttbarPlusCCbar', 'CMS_ttHbb_PDF_ttbarOther', 'CMS_ttHbb_PDF_ttbarPlusB', 'CMS_ttHbb_PDF_ttbarPlus2B', 'CMS_ttHbb_PDF_ttbarPlusBBbar', 'CMS_ttHbb_PDF_ttbarPlusCCbar', 'CMS_ttHbb_scaleMuF_ttbarOther', 'CMS_ttHbb_scaleMuF_ttbarPlusB', 'CMS_ttHbb_scaleMuF_ttbarPlus2B', 'CMS_ttHbb_scaleMuF_ttbarPlusBBbar', 'CMS_ttHbb_scaleMuF_ttbarPlusCCbar', 'CMS_ttHbb_scaleMuR_ttbarOther', 'CMS_ttHbb_scaleMuR_ttbarPlusB', 'CMS_ttHbb_scaleMuR_ttbarPlus2B', 'CMS_ttHbb_scaleMuR_ttbarPlusBBbar', 'CMS_ttHbb_scaleMuR_ttbarPlusCCbar']
+# filter non existent systs
+systs=[]
+#for asyst in asysts:
+    #if ("ISR" in asyst or "FSR" in asyst or "NNPDF" in asyst or "CMS_ttHbb_scale" in asyst):
+      #continue
+    #systs.append(asyst)  
+
+for p in procs:
+    for s in ["CMS_ttHbb_HDAMP","CMS_ttHbb_UE"]:
+        systs.append(s+"_"+p)
 
 #procs="ttH_hbb ttH_hcc ttH_hww ttH_hzz ttH_htt ttH_hgg ttH_hgluglu ttH_hzg ttbarOther ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar singlet wjets zjets ttbarW ttbarZ diboson".split(" ")
-procs="ttH ttbarOther ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar ttbarIncl singlet wjets zjets diboson ttbarZ ttbarW".split(" ")
-print procs
+#procs="ttH ttbarOther ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar ttbarIncl singlet wjets zjets diboson ttbarZ ttbarW".split(" ")
+
 
 inf=ROOT.TFile(infname,"READ")
 
@@ -64,6 +80,9 @@ for c in cats:
       if p=="ttbarIncl":
         up=inf.Get("ttbarOther"+"_"+discname+"_"+c+"_"+s+"Up")
         down=inf.Get("ttbarOther"+"_"+discname+"_"+c+"_"+s+"Down")
+        if up==None or down==None:
+            print "did not find", "ttbarOther"+"_"+discname+"_"+c+"_"+s+"Up"
+            continue
         for subp in "ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar".split(" "):
           subup=inf.Get(subp+"_"+discname+"_"+c+"_"+s+"Up")
           subdown=inf.Get(subp+"_"+discname+"_"+c+"_"+s+"Down")
@@ -74,69 +93,71 @@ for c in cats:
         down=inf.Get(p+"_"+discname+"_"+c+"_"+s+"Down")
       #print down, up
       if up!=None and down!=None:
-	up.SetLineColor(ROOT.kRed)
-	up.SetTitle("up")
-	down.SetTitle("down")
-	down.SetLineColor(ROOT.kCyan)
-	canvas=ROOT.TCanvas(p+"_"+discname+"_"+c+"_"+s,p+"_"+discname+"_"+c+"_"+s,800,600)
-	maxval=max(nom.GetMaximum(), max(up.GetMaximum(),down.GetMaximum()))
-	nom.SetMaximum(1.5*maxval)
-	nom.Draw("histoE")
-	nom.SetTitle(c+"_"+p+" "+s)
-	up.Draw("histoSameE")
-	down.Draw("histoSameE")
-	if doKStest:
+        up.SetLineColor(ROOT.kRed)
+        up.SetTitle("up")
+        down.SetTitle("down")
+        down.SetLineColor(ROOT.kCyan)
+        canvas=ROOT.TCanvas(p+"_"+discname+"_"+c+"_"+s,p+"_"+discname+"_"+c+"_"+s,800,600)
+        maxval=max(nom.GetMaximum(), max(up.GetMaximum(),down.GetMaximum()))
+        nom.SetMaximum(1.5*maxval)
+        nom.Draw("histoE")
+        nom.SetTitle(c+"_"+p+" "+s)
+        up.Draw("histoSameE")
+        down.Draw("histoSameE")
+        if doKStest:
           ksUp=nom.KolmogorovTest(up)
           ksDown=nom.KolmogorovTest(down)
-	integNom=nom.Integral()
-	integDown=down.Integral()
-	integUp=up.Integral()
-	fracUp=0
-	fracDown=0
-	if integNom!=0:
-	  fracDown=integDown/integNom
-	  fracUp=integUp/integNom
-		  
-	canvas.SetTitle(c+"_"+p+" "+s)
-	#canvas.BuildLegend()
-	legend=ROOT.TLegend()
-	legend.SetX1NDC(0.15)
-	legend.SetX2NDC(0.95)
-	legend.SetY1NDC(0.7)
-	legend.SetY2NDC(0.88)
-	legend.SetBorderSize(0);
-	legend.SetLineStyle(0);
-	legend.SetTextFont(42);
-	legend.SetTextSize(0.03);
-	legend.SetFillStyle(0);	
-	if doKStest:
-	  legend.AddEntry(nom,c+"_"+p+" (%.2f)" % integNom,"l")
+        integNom=nom.Integral()
+        integDown=down.Integral()
+        integUp=up.Integral()
+        fracUp=0
+        fracDown=0
+        if integNom!=0:
+          fracDown=integDown/integNom
+          fracUp=integUp/integNom
+                  
+        canvas.SetTitle(c+"_"+p+" "+s)
+        #canvas.BuildLegend()
+        legend=ROOT.TLegend()
+        legend.SetX1NDC(0.15)
+        legend.SetX2NDC(0.95)
+        legend.SetY1NDC(0.7)
+        legend.SetY2NDC(0.88)
+        legend.SetBorderSize(0);
+        legend.SetLineStyle(0);
+        legend.SetTextFont(42);
+        legend.SetTextSize(0.03);
+        legend.SetFillStyle(0);        
+        if doKStest:
+          legend.AddEntry(nom,c+"_"+p+" (%.2f)" % integNom,"l")
           legend.AddEntry(up,c+"_"+p+" Up"+" (%.2f, %.2f), KS-prob. %.2f" % (integUp,fracUp,ksUp) +"","l")
           legend.AddEntry(down,c+"_"+p+" Down"+" (%.2f, %.2f), KS-prob. %.2f" % (integDown,fracDown,ksDown) +"","l")
-	else:
+        else:
           legend.AddEntry(nom,c+"_"+p+" (%.2f)" % integNom,"l")
           legend.AddEntry(up,c+"_"+p+" Up"+" (%.2f, %.2f" % (integUp,fracUp) +")","l")
           legend.AddEntry(down,c+"_"+p+" Down"+" (%.2f, %.2f" % (integDown,fracDown) +")","l")
-	legend.Draw()
+        legend.Draw()
 
         canvas.Print(name+'.pdf')
-	canvas.SaveAs(name+"SystShapes/"+p+"_"+discname+"_"+c+"_"+s+".png")
-	canvas.SaveAs(name+"SystShapes/"+p+"_"+discname+"_"+c+"_"+s+".pdf")
-	ratioc=ROOT.TCanvas(p+"_"+discname+"_"+c+"_"+s,p+"_"+discname+"_"+c+"_"+s,800,300)
-	nomr=nom.Clone()
-	nomr.Divide(nom)
-	nomr.GetYaxis().SetRangeUser(0.5,1.5)
-	upr=up.Clone()
-	upr.Divide(nom)
-	downr=down.Clone()
-	downr.Divide(nom)
-	nomr.Draw("histoE")
-	upr.Draw("samehistoE")
-	downr.Draw("samehistoE")
-	ratioc.SetTitle(nom.GetTitle())
-	ratioc.SaveAs(name+"SystShapes/Ratio_"+p+"_"+discname+"_"+c+"_"+s+".png")
-	ratioc.SaveAs(name+"SystShapes/Ratio_"+p+"_"+discname+"_"+c+"_"+s+".pdf")
-	counter+=1
+        canvas.SaveAs(name+"SystShapes/"+p+"_"+discname+"_"+c+"_"+s+".png")
+        canvas.SaveAs(name+"SystShapes/"+p+"_"+discname+"_"+c+"_"+s+".pdf")
+        ratioc=ROOT.TCanvas(p+"_"+discname+"_"+c+"_"+s,p+"_"+discname+"_"+c+"_"+s,800,300)
+        nomr=nom.Clone()
+        nomr.Divide(nom)
+        nomr.GetYaxis().SetRangeUser(0.5,1.5)
+        upr=up.Clone()
+        upr.Divide(nom)
+        downr=down.Clone()
+        downr.Divide(nom)
+        nomr.Draw("histoE")
+        upr.Draw("samehistoE")
+        downr.Draw("samehistoE")
+        ratioc.SetTitle(nom.GetTitle())
+        ratioc.SaveAs(name+"SystShapes/Ratio_"+p+"_"+discname+"_"+c+"_"+s+".png")
+        ratioc.SaveAs(name+"SystShapes/Ratio_"+p+"_"+discname+"_"+c+"_"+s+".pdf")
+        counter+=1
+      else:
+          print "did not find", p+"_"+discname+"_"+c+"_"+s+"Up"
 buff.Print(name+'.pdf]')
-	
-	#exit(0)
+        
+        #exit(0)
