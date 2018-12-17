ca37805b42c38d99f75185b99fc27cab8754f20d
Michael Wassmer <michael.wassmer@cern.ch>
2018-12-17 14:14:09 +0100

    splitted muR and muF variations for different generators

--- MonoJet_cfg.py
+++ MonoJet_cfg.py
@@ -14,8 +14,8 @@ weightSystNames=["",
                  "_CMS_btag_cferr1Up","_CMS_btag_cferr1Down","_CMS_btag_cferr2Up","_CMS_btag_cferr2Down",
                  "_Weight_PUUp","_Weight_PUDown",
                  "_SFWeight_NormedUp","_SFWeight_NormedDown",
-                 "_Weight_scale_variation_muRUp","_Weight_scale_variation_muRDown","_Weight_scale_variation_muFUp","_Weight_scale_variation_muFDown",
-                 "_Weight_PDFUp","_Weight_PDFDown"
+                 #"_Weight_scale_variation_muRUp","_Weight_scale_variation_muRDown","_Weight_scale_variation_muFUp","_Weight_scale_variation_muFDown",
+                 #"_Weight_PDFUp","_Weight_PDFDown"
                  ]
 
 
@@ -43,12 +43,12 @@ systWeights=[   "NomWeight:="+common_weight+"*(DoWeights==1)+(DoWeights==0)*1.0"
                 "dummyWeight_PUdown:="+common_weight+"*Weight_pu69p2Down/Weight_pu69p2*(DoWeights==1)+(DoWeights==0)*1.0",
                 "dummyWeight_SFWeight_Normedup:="+common_weight+"*internalSFWeight_NormedUp*(DoWeights==1)+(DoWeights==0)*1.0",
                 "dummyWeight_SFWeight_Normeddown:="+common_weight+"*internalSFWeight_NormedDown*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_PDFup:="+common_weight+"*(Weight_LHA_292200_up>0.)*Weight_LHA_292200_up*(DoWeights==1)+(DoWeights==0)*1.0",
-                "dummyWeight_PDFdown:="+common_weight+"*(Weight_LHA_292200_down>0.)*Weight_LHA_292200_down*(DoWeights==1)+(DoWeights==0)*1.0"
+                #"dummyWeight_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+                #"dummyWeight_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+                #"dummyWeight_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+                #"dummyWeight_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0",
+                #"dummyWeight_PDFup:="+common_weight+"*(Weight_LHA_292200_up>0.)*Weight_LHA_292200_up*(DoWeights==1)+(DoWeights==0)*1.0",
+                #"dummyWeight_PDFdown:="+common_weight+"*(Weight_LHA_292200_down>0.)*Weight_LHA_292200_down*(DoWeights==1)+(DoWeights==0)*1.0"
                 ]
 otherSystNames=[
                     "_CMS_scale_jUp","_CMS_scale_jDown",
@@ -64,6 +64,9 @@ BosonSystNames=[
        "_BosonWeight_QCD2Up","_BosonWeight_QCD2Down",
        "_BosonWeight_QCD3Up","_BosonWeight_QCD3Down",
        "_BosonWeight_EW1Up","_BosonWeight_EW1Down",
+       
+       "_Weight_amc_scale_variation_muRUp","_Weight_amc_scale_variation_muRDown","_Weight_amc_scale_variation_muFUp","_Weight_amc_scale_variation_muFDown",
+       "_Weight_PDFUp","_Weight_PDFDown"
        #"_BosonWeight_EW2Up","_BosonWeight_EW2Down",
        #"_BosonWeight_EW3Up","_BosonWeight_EW3Down",
        #"_BosonWeight_MixedUp","_BosonWeight_MixedDown",
@@ -76,9 +79,16 @@ BosonWeights=[
        "dummyWeight_Boson_QCD2Down:="+common_weight+"*internalBosonWeight_QCD2Down/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
        "dummyWeight_Boson_QCD3Up:="+common_weight+"*internalBosonWeight_QCD3Up/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
        "dummyWeight_Boson_QCD3Down:="+common_weight+"*internalBosonWeight_QCD3Down/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
-
        "dummyWeight_Boson_EW1Up:="+common_weight+"*internalBosonWeight_EW1Up/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
        "dummyWeight_Boson_EW1Down:="+common_weight+"*internalBosonWeight_EW1Down/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
+       
+       "dummyWeight_amc_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+       "dummyWeight_amc_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+       "dummyWeight_amc_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+       "dummyWeight_amc_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0",
+       
+       "dummyWeight_PDFup:="+common_weight+"*(Weight_LHA_292200_up>0.)*Weight_LHA_292200_up*(DoWeights==1)+(DoWeights==0)*1.0",
+       "dummyWeight_PDFdown:="+common_weight+"*(Weight_LHA_292200_down>0.)*Weight_LHA_292200_down*(DoWeights==1)+(DoWeights==0)*1.0"
        #"dummyWeight_Boson_EW2Up:="+common_weight+"*internalBosonWeight_EW2Up/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
        #"dummyWeight_Boson_EW2Down:="+common_weight+"*internalBosonWeight_EW2Down/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
        #"dummyWeight_Boson_EW3Up:="+common_weight+"*internalBosonWeight_EW3Up/internalBosonWeight_nominal*(DoWeights==1)+(DoWeights==0)*1.0",
@@ -154,6 +164,50 @@ WBosonWeights=[
     
     ]
 
+PowhegSystNames=[
+    
+    "_Weight_powheg_scale_variation_muRUp","_Weight_powheg_scale_variation_muRDown","_Weight_powheg_scale_variation_muFUp","_Weight_powheg_scale_variation_muFDown"
+    
+    ]
+
+PowhegSystWeights=[
+    
+    "dummyWeight_powheg_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_powheg_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_powheg_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_powheg_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0"
+    
+    ]
+
+MadGraphSystNames=[
+    
+    "_Weight_madgraph_scale_variation_muRUp","_Weight_madgraph_scale_variation_muRDown","_Weight_madgraph_scale_variation_muFUp","_Weight_madgraph_scale_variation_muFDown"
+    
+    ]
+
+MadGraphSystWeights=[
+    
+    "dummyWeight_madgraph_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_madgraph_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_madgraph_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_madgraph_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0"
+    
+    ]
+
+SignalSystNames=[
+    
+    "_Weight_signal_scale_variation_muRUp","_Weight_signal_scale_variation_muRDown","_Weight_signal_scale_variation_muFUp","_Weight_signal_scale_variation_muFDown"
+    
+    ]
+
+SignalSystWeights=[
+    
+    "dummyWeight_signal_scale_variation_muRup:="+common_weight+"*internalBosonWeight_muRUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_2p0_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_signal_scale_variation_muRdown:="+common_weight+"*internalBosonWeight_muRDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_0p5_muF_1p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_signal_scale_variation_muFup:="+common_weight+"*internalBosonWeight_muFUp/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_2p0)*(DoWeights==1)+(DoWeights==0)*1.0",
+    "dummyWeight_signal_scale_variation_muFdown:="+common_weight+"*internalBosonWeight_muFDown/internalBosonWeight_nominal*fabs(Weight_scale_variation_muR_1p0_muF_0p5)*(DoWeights==1)+(DoWeights==0)*1.0"
+    
+    ]
 
 MCWeight='35.91823'
 
@@ -212,12 +266,12 @@ samples_background = [
                         #Sample('W(l#nu)+jets p_{T,W}=400-600',ROOT.kGreen+4,path_ntuples+'/WJetsToLNu_Pt-400To600*/*nominal*.root',"1.*"+MCWeight+sel_MET,'w_lnu_jets_c',weightSystNames+otherSystNames,samDict=sampleDict),
                         #Sample('W(l#nu)+jets p_{T,W}=600-Inf',ROOT.kGreen-6,path_ntuples+'/WJetsToLNu_Pt-600ToInf*/*nominal*.root',"1.*"+MCWeight+sel_MET,'w_lnu_jets_d',weightSystNames+otherSystNames,samDict=sampleDict),
                         
-                        Sample('Diboson',ROOT.kOrange,path_ntuples+'/??_TuneCUETP8M1_13TeV-pythia8/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'diboson',weightSystNames[:-6]+otherSystNames,samDict=sampleDict),
-                        Sample('Single Top',ROOT.kViolet-1,path_ntuples+'/ST_*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'singletop',weightSystNames[:-2]+otherSystNames,samDict=sampleDict),
-                        Sample('t#bar{t}',ROOT.kViolet-7,path_ntuples+'/TT_Tune*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'ttbar',weightSystNames[:-2]+otherSystNames,samDict=sampleDict),
+                        Sample('Diboson',ROOT.kOrange,path_ntuples+'/??_TuneCUETP8M1_13TeV-pythia8/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'diboson',weightSystNames+otherSystNames,samDict=sampleDict),
+                        Sample('Single Top',ROOT.kViolet-1,path_ntuples+'/ST_*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'singletop',weightSystNames+PowhegSystNames+otherSystNames,samDict=sampleDict),
+                        Sample('t#bar{t}',ROOT.kViolet-7,path_ntuples+'/TT_Tune*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'ttbar',weightSystNames+PowhegSystNames+otherSystNames,samDict=sampleDict),
                         Sample('Z(ll)+jets',ROOT.kCyan,path_ntuples+'/DYJetsToLL*amc*/*nominal*.root',"1.*0.971"+"*"+MCWeight+sel_MET,'z_ll_jets',weightSystNames+BosonSystNames+ZllBosonSystNames+otherSystNames,samDict=sampleDict),
-                        #Sample('QCD',ROOT.kGray,path_ntuples+'/QCD*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'qcd',weightSystNames[:-2]+otherSystNames,samDict=sampleDict),
-                        Sample('#gamma +jets',ROOT.kYellow+2,path_ntuples+'/GJets*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'gamma_jets',weightSystNames[:-2]+otherSystNames,samDict=sampleDict)
+                        #Sample('QCD',ROOT.kGray,path_ntuples+'/QCD*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'qcd',weightSystNames+MadGraphSystNames+otherSystNames,samDict=sampleDict),
+                        Sample('#gamma +jets',ROOT.kYellow+2,path_ntuples+'/GJets*/*nominal*.root',"1."+"*"+MCWeight+sel_MET,'gamma_jets',weightSystNames+MadGraphSystNames+otherSystNames,samDict=sampleDict)
                         ]
 """
 samples_signal = [
@@ -252,6 +306,6 @@ for i in range(len(signal_samples_names)):
     if xs == None:
         print "reading of signal cross section went wrong"
         exit()
-    samples_signal.append(Sample(signal_samples_names[i],ROOT.kRed,signal_samples_paths[i],"1.*"+xs+"*"+MCWeight+sel_MET,signal_samples_names[i],[""],samDict=sampleDict))
+    samples_signal.append(Sample(signal_samples_names[i],ROOT.kRed,signal_samples_paths[i],"1.*"+xs+"*"+MCWeight+sel_MET,signal_samples_names[i],weightSystNames+SignalSystNames+otherSystNames,samDict=sampleDict))
 #print samples_signal
 #print signal_samples_names
