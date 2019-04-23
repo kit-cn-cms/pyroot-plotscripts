066989945a306271cc5d052be9937b01f5013945
Jan van der Linden <jan.linden@student.kit.edu>
2019-04-23 16:22:25 +0200

    changed structure of systematics csv to read up/down variations with placeholder strings

--- configs/pltcfg_ttZ18.py
+++ configs/pltcfg_ttZ18.py
@@ -57,29 +57,28 @@ hzzSel='*((abs(GenHiggs_DecProd1_PDGID)==23 && abs(GenHiggs_DecProd2_PDGID)==23)
 # hzg with id 23 and id 22
 hzgSel='*((abs(GenHiggs_DecProd1_PDGID)==23 && abs(GenHiggs_DecProd2_PDGID)==22) || (abs(GenHiggs_DecProd1_PDGID)==22 && abs(GenHiggs_DecProd2_PDGID)==23))'
 
+# weights
+usualWeight = "(1*Weight_pu69p2*Weight_GEN_nom)"
+
+scalefactors = "internalEleIDWeight*internalMuIDWeight*internalMuIsoWeight*internalEleGFSWeight"
+
+mcTriggerWeight = "((1.0) * (internalEleTriggerWeight*(N_LooseMuons==0 && N_TightElectrons==1)* (Triggered_HLT_Ele35_WPTight_Gsf_vX==1 || Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX==1) +internalMuTriggerWeight*(N_LooseElectrons==0 && N_TightMuons==1)*(Muon_Pt[0]>29.) *(Triggered_HLT_IsoMu27_vX==1)))*(N_Jets>=4 && N_BTagsM>=3)"
+
+doWeightsFlag = "(DoWeights==1)+(DoWeights==0)*1.0"
+
+# dictionary of expressions to replace in systematics csv
+weightReplacements = {
+    "USUALWEIGHT":  usualWeight,
+    "SCALEFACTORS": scalefactors,
+    "MCWEIGHT":     mcTriggerWeight,
+    "DOWEIGHTS":    doWeightsFlag}
+
 # names of the systematics (proper names needed e.g. for combination)
-# TODO Add CSV SFs and uncertainties
 # Lumi weight
 mcWeight='59.7'
-
-# TODO Add Trigger SFs
-mcTriggerWeight='((1.0) * (internalEleTriggerWeight*(N_LooseMuons==0 && N_TightElectrons==1)* (Triggered_HLT_Ele35_WPTight_Gsf_vX==1 || Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX==1) +internalMuTriggerWeight*(N_LooseElectrons==0 && N_TightMuons==1)*(Muon_Pt[0]>29.) *(Triggered_HLT_IsoMu27_vX==1)))*(N_Jets>=4 && N_BTagsM>=3)'
-#mcTriggerWeight='(1.0)'
-#mcTriggerWeight='((1.0) * (1*(N_LooseMuons==0 && N_TightElectrons==1)* (1) +1*(N_LooseElectrons==0 && N_TightMuons==1) *(1)))*(N_Jets>=4 && N_BTagsM>=3)'
-
-#TODO Check that SFs and uncertainties are correct
-sfs="1.0"
-sfs="internalEleIDWeight*internalMuIDWeight*internalMuIsoWeight*internalEleGFSWeight"
-#sfs="internalEleIDWeight*internalMuIDWeight*internalMuIsoWeight*internalEleGFSWeight*internalMuHIPWeight"
-#sfs="1.0"
-
-usualWeights="(1*Weight_pu69p2*Weight_GEN_nom)"+"*"+sfs
-#usualWeights="(1*(Weight_GEN_nom))"+"*"+sfs
-
 evenSel="*(Evt_Odd==0)*2.0"
 
-nominalweight="NomWeight:="+usualWeights+"*"+mcTriggerWeight+"*internalCSVweight*1.0*(DoWeights==1)+(DoWeights==0)*1.0"
-
+nominalweight="NomWeight:="+usualWeight+"*"+scalefactors+"*"+mcTriggerWeight+"*internalCSVweight*1.0"+"*"+doWeightsFlag
 
 # data samples (name, color, path to files, selection, nickname_without_special_characters,optional: number of events for cross check)
 sampleDict=plotClasses.SampleDictionary()
