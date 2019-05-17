1f7a120efb6424a365408c2fbe8b9bb80ec1045d
Lea Reuter <lea.reuter@student.kit.edu>
2019-05-17 15:10:11 +0200

    WIP

--- util/tools/Plots.py
+++ util/tools/Plots.py
@@ -3,50 +3,190 @@ ROOT.gROOT.SetBatch(True)
 import re
 import numpy as np
 
+"""
+Class handling the Plotting
+Contains the Name and the Histogram
+label is for the legend naming
+if color is not set uses dictionary defined here
+OverUnderFlowInc as flag if Overflow and Underflow were already merged in the hist
+Errorband should be all uncertainties
+specific error band as list of error bands
+"""
 class Plot:
-	def __init__(self, histo, variable, label = '', color= '', typ=''):
-		self.histo=histo
-		self.variable=variable
+	def __init__(self, hist, name, label = None, 
+					color=None, typ='bkg', OverUnderFlowInc=False, 
+					errorband=None, specificerrorband=None):
+		self.hist=hist
+		self.name=name
 		self.label=label
+		if not label:
+			self.label=name
 		self.color=color
+		if not color:
+			self.GetPlotColor
 		self.typ=typ
+		self.OverUnderFlowInc=OverUnderFlowInc
+		self.errorband=errorband
+		self.specificerrorband=specificerrorband
+		if specificerrorband:
+			if not isinstance(specificerrorband, list):
+				self.specificerrorband=[specificerrorband]
+			self.sebColor=[ROOT.kCyan,ROOT.kYellow, 
+									ROOT.kMagenta,ROOT.kOrange,
+									ROOT.kPink, ROOT.kViolet]
+	# dictionary for colors
+	def GetPlotColor(self ):
+	    color_dict = {
+	        "ttZ":          ROOT.kCyan,
+	        "ttH":          ROOT.kBlue+1,
+	        "ttlf":         ROOT.kRed-7,
+	        "ttcc":         ROOT.kRed+1,
+	        "ttbb":         ROOT.kRed+3,
+	        "tt2b":         ROOT.kRed+2,
+	        "ttb":          ROOT.kRed-2,
+	        "tthf":         ROOT.kRed-3,
+	        "ttbar":        ROOT.kOrange,
+	        "ttmergedb":    ROOT.kRed-1,
+	    
+	        "sig":   ROOT.kCyan,
+	        "bkg":   ROOT.kOrange,
+	        }
+	    cls=self.name
+	    if "ttZ" in cls: cls = "ttZ"
+	    if "ttH" in cls: cls = "ttH"
+	    self.color=color_dict[cls]
 
+	def handleOverUnderFlow(self)
+		moveOverUnderFlow(self.hist)
+	    # set flag that over and underflow werde merged into the histogram
+	    self.OverUnderFlowInc=True
+
+	def setStyle(self):
+		#Sets style for Histogram, filled for background, line for signal
+		if self.typ=="bkg":
+			self.hist.SetLineColor(ROOT.kBlack )
+        	self.hist.SetFillColor(color)
+        	self.hist.SetLineWidth(1)
+		elif self.typ=="signal":
+			self.hist.SetLineColor( color )
+	        self.hist.SetFillColor(0)
+	        self.hist.SetLineWidth(2)
+		else:
+			print("ERROR! Type wrong!")
+		#sets style for error band
+		if self.errorband:
+			self.errorband.SetFillStyle(3004)
+			self.errorband.SetLineColor(ROOT.kBlack)
+			self.errorband.SetFillColor(ROOT.kBlack)
+		#sets style for error band
+		if self.specificerrorband:
+			for n,seb in enumerate(specificerrorband):
+				seb.SetFillStyle(3004)
+				seb.SetLineColor(self.sebColor[n])
+				seb.SetFillColor(self.sebColor[n])
+
+def buildHistogramAndErrorBand(rootfile,sample,nominalKey,systematicKey):
+	print("NEW SAMPLE"+str(sample.nick))
+    # replace keys to get histogram key
+    if procIden in nominalKey:
+        sampleKey = nominalKey.replace(procIden, sample.nick)
+    else:
+        sampleKey=nominalKey
+    rootHist = rootFile.Get(sampleKey)
+    #moves underflow in the first and overflow in the last bin
+    Plots.moveOverUnderFlow(rootHist)
+    print("type of hist is: "+str(type(rootHist)) )
+    if not isinstance(rootHist, ROOT.TH1):
+        continue
+
+    # replace keys to get systematic key
+    print("STARTING WITH SYSTEMATICS - building Errorband")
+    if procIden in systematicKey:
+        sampleSystKey = systematicKey.replace(procIden, sample.nick)
+    else:
+        sampleSystKey = systematicKey
+    #Lists for error band values
+	upErrors=None
+	downErrors=None
+    """
+    Loop over systematics to get Error band for sample
+    """
+    for systematic in sample.getShapes():
+        print systematic
+        """
+	    create empty Error Band for sample to fill 
+	    """
+	    if not upErrors and not downErrors:
+	        upErrors=[0]*rootHist.GetNbinsX()
+	        downErrors=[0]*rootHist.GetNbinsX()
+	    """
+	    Get Key for Sytematic (up and down variation)
+	    """
+        if sysIden in sampleSystKey:
+            sampleHistKey = sampleSystKey.replace(sysIden, systematic)
+        else:
+            sampleHistKey = sampleSystKey
+        upname=sampleHistKey+"Up"
+        up= rootFile.Get(upname)
+        downname=sampleHistKey+"Down"
+        down= rootFile.Get(downname)
+        
+        """
+        check if histogram is TH1 type, else skip uncertainty
+        """
+        if isinstance(up, ROOT.TH1) and isinstance(down, ROOT.TH1):
+            Plots.moveOverUnderFlow(up)
+            Plots.moveOverUnderFlow(down)
+
+            for ibin in range(0, rootHist.GetNbinsX()):
+                # get up down variations
+                u_=up[ibin+1]-rootHist[ibin+1]
+                d_=down[ibin+1]-rootHist[ibin+1]
+                 # set max as up and min as down
+                u = max(u_,d_)
+                d = min(u_,d_)
+                # only consider positive up and negative down variations
+                u = max(0.,u)
+                d = min(0.,d)
+                upErrors[ibin]=ROOT.TMath.Sqrt( upErrors[ibin]*upErrors[ibin] + u*u )
+                downErrors[ibin] = ROOT.TMath.Sqrt( downErrors[ibin]*downErrors[ibin] + d*d)
+                if debug>99:
+                    print "adding up/down ", u, d
+                    print "total up/down now: ", upErrors[ibin], downErrors[ibin]
+                    print "-"*50
+        else:
+            print("ERROR! can not use: "+str(systematic) )
+            print("->type of up shape hist is: "+str(type(up)) )
+            print("->type of down shape is: "+str(type(down)) )
+
+    if upErrors:
+    	eb = ROOT.TGraphAsymmErrors(hist)
+    	for i in range(len(upErrors)):
+		    eb.SetPointEYlow(i, downErrors[i])
+		    eb.SetPointEYhigh(i, upErrors[i])
+		    eb.SetPointEXlow(i, AllHists.GetBinWidth(i+1)/2.)
+		    eb.SetPointEXhigh(i, AllHists.GetBinWidth(i+1)/2.)
+  		Plot=Plot(rootHist,sample.nick,label=sample.name,
+  									color=sample.color,typ=sample.typ,
+  									errorband=eb)
+  	else:
+  		Plot=Plot(rootHist,sample.nick,label=sample.name,
+  									color=sample.color,typ=sample.typ)
+  	return Plot
 
 def moveOverUnderFlow(hist):
-    # move underflow
-    hist.SetBinContent(1, hist.GetBinContent(0)+hist.GetBinContent(1))
-    # move overflow
-    hist.SetBinContent(hist.GetNbinsX(), hist.GetBinContent(hist.GetNbinsX()+1)+hist.GetBinContent(hist.GetNbinsX()))
+	    # move underflow
+	    hist.SetBinContent(1, hist.GetBinContent(0)+hist.GetBinContent(1))
+	    # move overflow
+	    hist.SetBinContent(hist.GetNbinsX(), hist.GetBinContent(hist.GetNbinsX()+1)+hist.GetBinContent(hist.GetNbinsX()))
 
-    # set underflow error
-    hist.SetBinError(1, ROOT.TMath.Sqrt(
-        ROOT.TMath.Power(hist.GetBinError(0),2) + ROOT.TMath.Power(hist.GetBinError(1),2) ))
-    # set overflow error
-    hist.SetBinError(hist.GetNbinsX(), ROOT.TMath.Sqrt(
-ROOT.TMath.Power(hist.GetBinError(hist.GetNbinsX()),2) + ROOT.TMath.Power(hist.GetBinError(hist.GetNbinsX()+1),2) ))
-
-
-# dictionary for colors
-def GetPlotColor( cls ):
-    color_dict = {
-        "ttZ":          ROOT.kCyan,
-        "ttH":          ROOT.kBlue+1,
-        "ttlf":         ROOT.kRed-7,
-        "ttcc":         ROOT.kRed+1,
-        "ttbb":         ROOT.kRed+3,
-        "tt2b":         ROOT.kRed+2,
-        "ttb":          ROOT.kRed-2,
-        "tthf":         ROOT.kRed-3,
-        "ttbar":        ROOT.kOrange,
-        "ttmergedb":    ROOT.kRed-1,
-    
-        "sig":   ROOT.kCyan,
-        "bkg":   ROOT.kOrange,
-        }
-
-    if "ttZ" in cls: cls = "ttZ"
-    if "ttH" in cls: cls = "ttH"
-    return color_dict[cls]
+	    # set underflow error
+	    hist.SetBinError(1, ROOT.TMath.Sqrt(
+	        ROOT.TMath.Power(hist.GetBinError(0),2) + ROOT.TMath.Power(hist.GetBinError(1),2) ))
+	    # set overflow error
+	    hist.SetBinError(hist.GetNbinsX(), ROOT.TMath.Sqrt(
+			ROOT.TMath.Power(hist.GetBinError(hist.GetNbinsX()),2) + 
+			ROOT.TMath.Power(hist.GetBinError(hist.GetNbinsX()+1),2) ))
 
 def GetyTitle(privateWork = False):
     # if privateWork flag is enabled, normalize plots to unit area
