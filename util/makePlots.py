80a86c00c7788655563de70c8a023d277f2d0956
Lea Reuter <lea.reuter@student.kit.edu>
2019-06-06 14:58:07 +0200

    WIP

--- util/makePlots.py
+++ util/makePlots.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import configClass as configClass
+import analysisClass as analysisClass
 
 filedir = os.path.dirname(os.path.realpath(__file__))
 pyrootdir = "/".join(filedir.split("/")[:-1])
@@ -8,47 +10,47 @@ sys.path.append(pyrootdir+"/configs")
 
 
 
-
-def makePlots(ListOfPlots, workdir):
-
-    print ListOfPlots
+def makePlots(configData):
+    ListOfPlots=configData.getDiscriminatorPlots()
+    workdir=configData.analysis.workdir
+    plot_config=configData.plot_config
+    syst_config=configData.systconfig
     print('creating output folders')
-    scriptPath = workdir+'/DrawScripts/'
+    scriptPath = workdir+'/PlotScripts/'
     #if not os.path.exists(scriptPath):
     #    os.makedirs(scriptPath)
     ListOfScripts = []
     #print('Creating Scripts for Parallel Drawing')
-    for iPlot, Plot in enumerate(ListOfPlots):
+    for Plot in ListOfPlots:
         print "-"*130
-        print iPlot
-        print Plot
         print Plot.name
-        print Plot.histo
-        print Plot.variable
-        print Plot.selection
-        print Plot.label
-        #ListOfScripts.append( createDrawScript(iPlot, Plot, scriptPath, opts=None) )
+        ListOfScripts.append( createPlotScript(channel=Plot.name, workdir=workdir,plotconfig=plot_config, systconfig=syst_config) )
 
     #print "Submitting ", len(ListOfScripts), " DrawScripts"
     #nafInterface.drawInterface(ListOfScripts, ListOfPlots)
 
     return
 
-def createDrawScript(iPlot, Plot, scriptPath, opts=None):
-    
+def createPlotScript(channel, workdir,plotconfig,systconfig):
+    scriptPath = workdir+'/DrawScripts/'
+    pathtoself=workdir+'/util/'
     cmsswpath = os.environ['CMSSW_BASE']
     script="#!/bin/bash \n"
     if cmsswpath != '':
         script += "export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch \n"
         script += "source $VO_CMS_SW_DIR/cmsset_default.sh \n"
         script += "export SCRAM_ARCH="+os.environ['SCRAM_ARCH']+"\n"
-        script += 'export OUTFILENAME="'+"plot" +str(iPlot)+'"\n'
+        script += 'export OUTFILENAME="'+"plot" +str(channel)+'"\n'
         script += 'cd '+cmsswpath+'/src\n'
         script += 'eval `scram runtime -sh`\n'
         script += 'cd - \n'
 
-    script += 'python '+scriptPath+" -p "+str(iPlot)+ ' '  + ' noPlotParallel\n'
+    script += 'python '+pathtoself+'PlotScript.py --plotconfig="'+plotconfig+'" '
+    script += ' --channelname="'+channel+'" '
+    script += ' --rootfile="/nfs/dust/cms/user/lreuter/forPhilip/plotscript/workdir/csvConfigAll/output_limitInput.root" '
+    script += ' --directory="'+workdir+'"' 
+    script += ' --systconfig="'+systconfig+'"\n'
 
-    scriptPath = scriptPath+'DrawParallel'+str(iPlot)+'.sh'
+    scriptPath = scriptPath+'makePlots'+str(channel)+'.sh'
     print script
 
