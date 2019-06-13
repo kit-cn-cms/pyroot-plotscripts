4b9cc8c57a64c4ad5495309b40d5852db2783d85
Lea Reuter <lea.reuter@student.kit.edu>
2019-06-13 11:34:23 +0200

    makes plotting scripts

--- util/makePlots.py
+++ util/makePlots.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import stat
 import configClass as configClass
 import analysisClass as analysisClass
 
@@ -7,33 +8,41 @@ filedir = os.path.dirname(os.path.realpath(__file__))
 pyrootdir = "/".join(filedir.split("/")[:-1])
 
 sys.path.append(pyrootdir+"/configs")
+sys.path.append(filedir+"/tools")
+import nafInterface
 
 
 
 def makePlots(configData):
     ListOfPlots=configData.getDiscriminatorPlots()
     workdir=configData.analysis.workdir
-    plot_config=configData.plot_config
+    config=configData.analysis.plotConfig
+    #plot_config=configData.plot_config
     syst_config=configData.systconfig
+    pyrootdir=configData.analysis.pyrootdir
+
+    # create output folders
     print('creating output folders')
     scriptPath = workdir+'/PlotScripts/'
-    #if not os.path.exists(scriptPath):
-    #    os.makedirs(scriptPath)
+    if not os.path.exists(scriptPath):
+        os.makedirs(scriptPath)
+
     ListOfScripts = []
     #print('Creating Scripts for Parallel Drawing')
     for Plot in ListOfPlots:
         print "-"*130
         print Plot.name
-        ListOfScripts.append( createPlotScript(channel=Plot.name, workdir=workdir,plotconfig=plot_config, systconfig=syst_config) )
+        ListOfScripts.append( createPlotScript(channel=Plot.name,pyrootdir=pyrootdir, 
+        										workdir=workdir, scriptPath=scriptPath,plotconfig=config,
+        										systconfig=syst_config) )
 
-    #print "Submitting ", len(ListOfScripts), " DrawScripts"
-    #nafInterface.drawInterface(ListOfScripts, ListOfPlots)
+    print "Submitting ", len(ListOfScripts), " DrawScripts"
+    nafInterface.drawInterface(ListOfScripts, ListOfPlots)
 
     return
 
-def createPlotScript(channel, workdir,plotconfig,systconfig):
-    scriptPath = workdir+'/DrawScripts/'
-    pathtoself=workdir+'/util/'
+def createPlotScript(channel,pyrootdir,workdir,scriptPath,plotconfig,systconfig):
+    pathtoself=pyrootdir+'/util/'
     cmsswpath = os.environ['CMSSW_BASE']
     script="#!/bin/bash \n"
     if cmsswpath != '':
@@ -48,9 +57,19 @@ def createPlotScript(channel, workdir,plotconfig,systconfig):
     script += 'python '+pathtoself+'PlotScript.py --plotconfig="'+plotconfig+'" '
     script += ' --channelname="'+channel+'" '
     script += ' --rootfile="/nfs/dust/cms/user/lreuter/forPhilip/plotscript/workdir/csvConfigAll/output_limitInput.root" '
-    script += ' --directory="'+workdir+'"' 
+    script += ' --directory="'+pyrootdir+'"' 
+    script += ' --workdir="'+workdir+'"' 
     script += ' --systconfig="'+systconfig+'"\n'
 
     scriptPath = scriptPath+'makePlots'+str(channel)+'.sh'
-    print script
+
+    # write and chmod shell scripts
+    with open(scriptPath, "w") as sf:
+        sf.write(script)
+    st = os.stat(scriptPath)
+    os.chmod(scriptPath, st.st_mode | stat.S_IEXEC)
+    os.chdir(os.path.dirname(pathtoself))
+
+    return scriptPath
+  
 
