6ab5519a9d2ab491201792a1bbaad2bac51a3486
Jan van der Linden <jan.linden@student.kit.edu>
2019-07-22 08:44:27 +0200

    fit top level scripts to new config structure

--- plottingscripts/plotLimits_ttH18.py
+++ plottingscripts/plotLimits_ttH18.py
@@ -4,7 +4,7 @@ import os
 import imp
 import inspect
 import ROOT
-import opts
+import optparse
 
 filedir = os.path.dirname(os.path.realpath(__file__))
 pyrootdir = "/".join(filedir.split("/")[:-1])
@@ -48,10 +48,10 @@ def main(pyrootdir, opts):
     memexp = '(memDBp>=0.0)*(memDBp)+(memDBp<0.0)*(0.01)+(memDBp==1.0)*(0.01)'
 
     # configs
-    config          = "pltcfg_ttH18_Jan"
-    variable_cfg    = "ttH18_addVariables"
-    plot_cfg        = "ttH18_discrPlots"
-    syst_cfg        = "ttZ18_systematics"
+    config          = "ttH18/pltcfg_Jan"
+    variable_cfg    = "ttH18/additionalVariables"
+    plot_cfg        = "ttH18/discrPlots"
+    syst_cfg        = "ttZ18/systematics"
 
     # file for rate factors
     #rateFactorsFile = pyrootdir + "/data/rate_factors_onlyinternal_powhegpythia.csv"
