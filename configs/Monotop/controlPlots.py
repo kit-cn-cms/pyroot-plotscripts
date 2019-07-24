98b50fcadbe5851835bb243943afc3a139dacb06
Michael Wassmer <michael.wassmer@cern.ch>
2019-07-24 17:17:23 +0200

    update monotop plotting configs

--- configs/Monotop/controlPlots.py
+++ configs/Monotop/controlPlots.py
@@ -15,34 +15,57 @@ from copy import deepcopy
 
 def evtYieldCategories():
     return [
-        ("(N_LooseMuons==0 && N_LooseElectrons==0 && N_LoosePhotons==0)", "lepton veto", ""),
+        (
+            "(N_LooseMuons==0 && N_LooseElectrons==0 && N_LoosePhotons==0)",
+            "Puppi MET > 250, AK15 Jet Pt > 250, lepton veto",
+            "",
+        )
     ]
 
 
-
 def control_plots(data=None):
-    label = "lepton veto"
+    label = "Puppi MET > 250, AK15 Jet Pt > 250, lepton veto"
     selection = "(N_LooseMuons==0 && N_LooseElectrons==0 && N_LoosePhotons==0)"
 
     plots = [
         plotClasses.Plot(
-            ROOT.TH1D("Evt_Pt_MET", "Puppi MET", 20, 200.0, 700.0),
+            ROOT.TH1D("Evt_Pt_MET", "Puppi MET", 32, 200.0, 1000.0),
             "Evt_Pt_MET",
             selection,
             label,
         ),
         plotClasses.Plot(
-            ROOT.TH1D("AK15Jet_Pt", "AK15 Jet pt", 20, 200.0, 700.0),
+            ROOT.TH1D("AK15Jet_Pt", "AK15 Jet p_{t}", 32, 200.0, 1000.0),
             "AK15Jet_Pt",
             selection,
             label,
         ),
         plotClasses.Plot(
-            ROOT.TH1D("AK15Jet_DeepAK15_TvsQCD", "AK15Jet_DeepAK15_TvsQCD", 20, 0., 1.),
+            ROOT.TH1D(
+                "AK15Jet_DeepAK15_TvsQCD", "AK15 Jet DeepAK15 TvsQCD", 20, 0.0, 1.0
+            ),
             "AK15Jet_DeepAK15_TvsQCD",
             selection,
             label,
-        )
+        ),
+        plotClasses.Plot(
+            ROOT.TH1D(
+                "AK15Jet_Njettiness_tau3_AK15Jet_Njettiness_tau2",
+                "AK15 Jet #tau_{3}/#tau_{2}",
+                20,
+                0.0,
+                1.0,
+            ),
+            "AK15Jet_Njettiness_tau3/AK15Jet_Njettiness_tau2",
+            selection,
+            label,
+        ),
+        plotClasses.Plot(
+            ROOT.TH1D("AK15Jet_PuppiSoftDropMass", "AK15 Jet SD mass", 20, 50.0, 250.0),
+            "AK15Jet_PuppiSoftDropMass",
+            selection,
+            label,
+        ),
     ]
     if data:
         add_data_plots(plots=plots, data=data)
@@ -52,15 +75,15 @@ def control_plots(data=None):
 
 def getDiscriminatorPlots(data=None, discrname=""):
     discriminatorPlots = []
-    #discriminatorPlots += plots_ge4j_ge4t(data)
-    #discriminatorPlots += plots_ge6j_ge3t(data)
-    #discriminatorPlots += plots_le5j_ge3t(data)
-    #discriminatorPlots += plots_5j_ge3t(data)
-    #discriminatorPlots += plots_ge4j_ge3t(data)
-    #discriminatorPlots += plots_4j_ge3t(data)
-    #discriminatorPlots += plots_ge4j_3t(data)
+    # discriminatorPlots += plots_ge4j_ge4t(data)
+    # discriminatorPlots += plots_ge6j_ge3t(data)
+    # discriminatorPlots += plots_le5j_ge3t(data)
+    # discriminatorPlots += plots_5j_ge3t(data)
+    # discriminatorPlots += plots_ge4j_ge3t(data)
+    # discriminatorPlots += plots_4j_ge3t(data)
+    # discriminatorPlots += plots_ge4j_3t(data)
     discriminatorPlots += control_plots(data)
-    #discriminatorPlots += reco_plots(data)
+    # discriminatorPlots += reco_plots(data)
 
     return discriminatorPlots
 
