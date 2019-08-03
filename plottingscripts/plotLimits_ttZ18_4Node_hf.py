1c85d3a14bca9788542ef2b488a75b8b03e009db
Jan van der Linden <jan.linden@student.kit.edu>
2019-08-03 09:57:45 +0200

    added ratefactors for 2018 samples and updated ttZ configs

--- plottingscripts/plotLimits_ttZ18_4Node_hf.py
+++ plottingscripts/plotLimits_ttZ18_4Node_hf.py
@@ -56,9 +56,7 @@ def main(pyrootdir, opts):
     syst_cfg        = "ttZ18/systematics_internalCSV_hf"
 
     # file for rate factors1
-    #rateFactorsFile = pyrootdir + "/data/rate_factors_onlyinternal_powhegpythia.csv"
-    #rateFactorsFile = "/nfs/dust/cms/user/kelmorab/DataFilesForScriptGenerator/Summer18_2017data/rate_factors_V2.csv"
-    rateFactorsFile = "/nfs/dust/cms/user/vdlinden/legacyTTH/ratefactors/ratefactors_2018.csv"
+    rateFactorsFile = pyrootdir+"/data/rateFactors/rateFactors_2018.csv"
 
     # script options
     analysisOptions = {
