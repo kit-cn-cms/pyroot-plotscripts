591074b4d34a391838c499d903e7041a4616e602
Jan van der Linden <jan.linden@student.kit.edu>
2019-08-11 22:17:25 +0200

    ttZ config updates

--- configs/ttZ18/pltcfg_discrPlots_internalCSV_hf.py
+++ configs/ttZ18/pltcfg_discrPlots_internalCSV_hf.py
@@ -11,7 +11,7 @@ import util.tools.plotClasses as plotClasses
 
 # samples
 # input path 
-path_vdlinden  = "/nfs/dust/cms/user/vdlinden/legacyTTH/ntuples/legacy_2018_ttZ_v2"
+path_vdlinden  = "/nfs/dust/cms/user/vdlinden/legacyTTH/ntuples/legacy_2018_ttZ_v4"
 
 ttbarPathS = path_vdlinden+'/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*nominal*.root'+';'+ \
              path_vdlinden+'/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/*nominal*.root'+';'+\
@@ -40,8 +40,8 @@ ttZpath =  path_vdlinden+'/TTZToQQ*/*nominal*.root'+';'+ \
 # SELECTIONS
 
 # need to veto muon events in electron dataset to avoid double counting and vice versa
-sel_singleel="(N_LooseMuons==0 && N_TightElectrons==1 && (Triggered_HLT_Ele32_WPTight_Gsf_vX==1))"
-sel_singlemu="(N_LooseElectrons==0 && N_TightMuons==1 && (Triggered_HLT_IsoMu24_vX==1))"
+sel_singleel="(N_LooseMuons==0 && N_TightElectrons==1)"# && (Triggered_HLT_Ele32_WPTight_Gsf_vX==1 || Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX==1)"
+sel_singlemu="(N_LooseElectrons==0 && N_TightMuons==1)"#&& (Triggered_HLT_IsoMu24_vX==1))"
 
 # jet tag base selection
 sel_jettag = "(N_Jets>=4 && N_BTagsM>=3)"
@@ -172,19 +172,19 @@ samples = [
     plotClasses.Sample('t#bar{t}Z(b#bar{b})',ROOT.kOrange+7,
             path_vdlinden+'/TTZToBB*/*nominal*.root',
             # lumi reweighting factor due to stupid cross section calculation
-            lumi+"*1.087"+evenSel+sel_MET,
+            lumi+"*1.014"+evenSel+sel_MET,
             'ttZbb',
             samDict=sampleDict, readTrees=doReadTrees, typ = "signal"),
 
     plotClasses.Sample('t#bar{t}Z(q#bar{q})',ROOT.kOrange+7,
             path_vdlinden+'/TTZToQQ*/*nominal*.root',
-            lumi+evenSel+"*(GenEvt_I_TTZ==0)"+sel_MET,
+            lumi+"*1.014"+evenSel+"*(GenEvt_I_TTZ==0)"+sel_MET,
             'ttZqq',
             samDict=sampleDict, readTrees=doReadTrees, typ = "signal"),
 
     plotClasses.Sample('t#bar{t}Z(ll)', ROOT.kOrange+7,
             path_vdlinden+'/TTZToLLNuNu_M-10*/*nominal*.root',
-            lumi+"*1.006"+evenSel+sel_MET,
+            lumi+"*1.014"+evenSel+sel_MET,
             'ttZll',
             samDict=sampleDict, readTrees=doReadTrees, typ = "signal"),
 
