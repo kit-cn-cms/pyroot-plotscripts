5f26b5265c9ff9a4d71b347f92aa5a4aa757eccd
sebwieland <wielandseb@googlemail.com>
2019-09-20 10:46:15 +0200

    stupid..

--- configs/ttH17_legacy/pltcfg_2017_combined_tthf.py
+++ configs/ttH17_legacy/pltcfg_2017_combined_tthf.py
@@ -35,7 +35,7 @@ THWpath = path+'/THW_*ctcvcp*/*nominal*.root'
 THQpath = path+'/THQ_*ctcvcp*/*nominal*.root'
 
 
-ttHpath = path+'/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/*pythia8_16*nominal*.root'+';'+ \
+ttHpath = path+'/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/*nominal*.root'+';'+ \
 	  path+'/ttHToNonbb_M125*/*nominal*.root'
 
 ttZpath =  path+'/TTZToQQ*/*nominal*.root'+';'+ \
