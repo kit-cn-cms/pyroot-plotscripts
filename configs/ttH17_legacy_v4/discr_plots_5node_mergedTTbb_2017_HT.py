0d077cc5e137c553606123d21e5aafb874f55129
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2019-11-08 15:33:56 +0100

    changed 'discr' to 'varname' to make things clearer

--- configs/ttH17_legacy_v4/discr_plots_5node_mergedTTbb_2017_HT.py
+++ configs/ttH17_legacy_v4/discr_plots_5node_mergedTTbb_2017_HT.py
@@ -61,7 +61,7 @@ def plots_ge4j_3t(data = None):
     #     # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_Reco_ttbar_btophad_eta","Reco_ttbar_btophad_eta",50,-4.0,4.0),"Reco_ttbar_btophad_eta",selection,label),
     #     ]
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.))","ljets_ge4j_3t_Evt_HT","")
-    category_dict["discr"] = "Evt_HT"
+    category_dict["varname"] = "Evt_HT"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,230.0,260.0,290.0,320.0,350.0,380.0,410.0,440.0,470.0,500.0,530.0,560.0,590.0,620.0,650.0,680.0,710.0,740.0,770.0,800.0,830.0,860.0,890.0,920.0,950.0,980.0,1010.0,1040.0,1070.0,1100.0,1130.0,1160.0,1190.0,1220.0,1250.0,1280.0,1310.0,1340.0,1370.0,1400.0,1430.0,1460.0,1490.0,1520.0,1550.0,1580.0,1610.0,1640.0,1670.0,1700.0]
@@ -73,7 +73,7 @@ def plots_ge4j_3t(data = None):
     category_dict.clear()
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.))","ljets_ge4j_3t_Evt_HT_jets","")
-    category_dict["discr"] = "Evt_HT_jets"
+    category_dict["varname"] = "Evt_HT_jets"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,226.0,252.0,278.0,304.0,330.0,356.0,382.0,408.0,434.0,460.0,486.0,512.0,538.0,564.0,590.0,616.0,642.0,668.0,694.0,720.0,746.0,772.0,798.0,824.0,850.0,876.0,902.0,928.0,954.0,980.0,1006.0,1032.0,1058.0,1084.0,1110.0,1136.0,1162.0,1188.0,1214.0,1240.0,1266.0,1292.0,1318.0,1344.0,1370.0,1396.0,1422.0,1448.0,1474.0,1500.0]
@@ -85,7 +85,7 @@ def plots_ge4j_3t(data = None):
     category_dict.clear()
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.))","ljets_ge4j_3t_Evt_HT_wo_MET","")
-    category_dict["discr"] = "Evt_HT_wo_MET"
+    category_dict["varname"] = "Evt_HT_wo_MET"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,226.0,252.0,278.0,304.0,330.0,356.0,382.0,408.0,434.0,460.0,486.0,512.0,538.0,564.0,590.0,616.0,642.0,668.0,694.0,720.0,746.0,772.0,798.0,824.0,850.0,876.0,902.0,928.0,954.0,980.0,1006.0,1032.0,1058.0,1084.0,1110.0,1136.0,1162.0,1188.0,1214.0,1240.0,1266.0,1292.0,1318.0,1344.0,1370.0,1396.0,1422.0,1448.0,1474.0,1500.0]
@@ -149,7 +149,7 @@ def plots_ge4j_ge4t(data = None):
     #     ]
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.))","ljets_ge4j_ge4t_Evt_HT","")
-    category_dict["discr"] = "Evt_HT"
+    category_dict["varname"] = "Evt_HT"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,260.0,290.0,320.0,350.0,380.0,410.0,440.0,470.0,500.0,530.0,560.0,590.0,620.0,650.0,680.0,710.0,740.0,770.0,800.0,830.0,860.0,890.0,920.0,950.0,980.0,1010.0,1040.0,1070.0,1100.0,1130.0,1160.0,1190.0,1220.0,1250.0,1280.0,1310.0,1340.0,1370.0,1400.0,1460.0,1520.0,1580.0,1670.0,1700.0]
@@ -161,7 +161,7 @@ def plots_ge4j_ge4t(data = None):
     category_dict.clear()
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.))","ljets_ge4j_ge4t_Evt_HT_jets","")
-    category_dict["discr"] = "Evt_HT_jets"
+    category_dict["varname"] = "Evt_HT_jets"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,226.0,252.0,278.0,304.0,330.0,356.0,382.0,408.0,434.0,460.0,486.0,512.0,538.0,564.0,590.0,616.0,642.0,668.0,694.0,720.0,746.0,772.0,798.0,824.0,850.0,876.0,902.0,928.0,954.0,980.0,1006.0,1032.0,1058.0,1084.0,1110.0,1162.0,1188.0,1240.0,1292.0,1344.0,1396.0,1474.0,1500.0]
@@ -173,7 +173,7 @@ def plots_ge4j_ge4t(data = None):
     category_dict.clear()
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.))","ljets_ge4j_ge4t_Evt_HT_wo_MET","")
-    category_dict["discr"] = "Evt_HT_wo_MET"
+    category_dict["varname"] = "Evt_HT_wo_MET"
     category_dict["catlabel"] = label
     category_dict["nhistobins"] = ndefaultbins
     category_dict["bin_edges"] = [200.0,226.0,252.0,278.0,304.0,330.0,356.0,382.0,408.0,434.0,460.0,486.0,512.0,538.0,564.0,590.0,616.0,642.0,668.0,694.0,720.0,746.0,772.0,798.0,824.0,850.0,876.0,902.0,928.0,954.0,980.0,1006.0,1032.0,1058.0,1084.0,1110.0,1136.0,1162.0,1188.0,1214.0,1240.0,1292.0,1318.0,1370.0,1422.0,1474.0,1500.0]
@@ -205,7 +205,7 @@ def plots_dnn(data, discrname):
     # plots for ge4j_3t
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t==0))","ljets_ge4j_3t_ttH_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_3t_node_ttH"
+    category_dict["varname"] = "DNNOutput_ge4j_3t_node_ttH"
     category_dict["catlabel"] = "\geq 4 jets, 3 b-tags"
     category_dict["nhistobins"] = ndefaultbins
     category_dict["minxval"] = 0.2
@@ -215,7 +215,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t==1))","ljets_ge4j_3t_ttb_bb_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_3t_node_ttb_bb"
+    category_dict["varname"] = "DNNOutput_ge4j_3t_node_ttb_bb"
     category_dict["catlabel"] = "\geq 4 jets, 3 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -225,7 +225,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t==2))","ljets_ge4j_3t_tt2b_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_3t_node_tt2b"
+    category_dict["varname"] = "DNNOutput_ge4j_3t_node_tt2b"
     category_dict["catlabel"] = "\geq 4 jets, 3 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -235,7 +235,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t==3))","ljets_ge4j_3t_ttcc_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_3t_node_ttcc"
+    category_dict["varname"] = "DNNOutput_ge4j_3t_node_ttcc"
     category_dict["catlabel"] = "\geq 4 jets, 3 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -245,7 +245,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t==4))","ljets_ge4j_3t_ttlf_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_3t_node_ttlf"
+    category_dict["varname"] = "DNNOutput_ge4j_3t_node_ttlf"
     category_dict["catlabel"] = "\geq 4 jets, 3 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -259,7 +259,7 @@ def plots_dnn(data, discrname):
     # plots for ge4j_ge4t
 
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.)&&(DNNPredictedClass_ge4j_ge4t==0))","ljets_ge4j_ge4t_ttH_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_ge4t_node_ttH"
+    category_dict["varname"] = "DNNOutput_ge4j_ge4t_node_ttH"
     category_dict["catlabel"] = "\geq 4 jets, \geq 4 b-tags"
     category_dict["nhistobins"] = ndefaultbins
     category_dict["minxval"] = 0.2
@@ -269,7 +269,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.)&&(DNNPredictedClass_ge4j_ge4t==1))","ljets_ge4j_ge4t_ttb_bb_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_ge4t_node_ttb_bb"
+    category_dict["varname"] = "DNNOutput_ge4j_ge4t_node_ttb_bb"
     category_dict["catlabel"] = "\geq 4 jets, \geq 4 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -279,7 +279,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.)&&(DNNPredictedClass_ge4j_ge4t==2))","ljets_ge4j_ge4t_tt2b_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_ge4t_node_tt2b"
+    category_dict["varname"] = "DNNOutput_ge4j_ge4t_node_tt2b"
     category_dict["catlabel"] = "\geq 4 jets, \geq 4 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -289,7 +289,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.)&&(DNNPredictedClass_ge4j_ge4t==3))","ljets_ge4j_ge4t_ttcc_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_ge4t_node_ttcc"
+    category_dict["varname"] = "DNNOutput_ge4j_ge4t_node_ttcc"
     category_dict["catlabel"] = "\geq 4 jets, \geq 4 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -299,7 +299,7 @@ def plots_dnn(data, discrname):
     category_dict.clear()
     
     category_dict["category"] = ("((N_Jets>=4&&N_BTagsM>=4)&&(1.)&&(DNNPredictedClass_ge4j_ge4t==4))","ljets_ge4j_ge4t_ttlf_node","")
-    category_dict["discr"] = "DNNOutput_ge4j_ge4t_node_ttlf"
+    category_dict["varname"] = "DNNOutput_ge4j_ge4t_node_ttlf"
     category_dict["catlabel"] = "\geq 4 jets, \geq 4 b-tags"
     category_dict["nhistobins"] = 1
     category_dict["minxval"] = 0.2
@@ -331,7 +331,7 @@ def init_plots(dictionary, data = None):
     plots = [] #init list of plotClasses objects to return
     for label in dictionary:
         subdict     = dictionary[label] #for easy access
-        discr       = subdict["discr"] # load discriminator name
+        varname     = subdict["varname"] # load discriminator name
         sel         = subdict["plotPreselections"] # load selection
         histoname   = subdict["histoname"] # load histogram name
         histotitle  = subdict["histotitle"] # load histogram title
@@ -348,7 +348,7 @@ def init_plots(dictionary, data = None):
             plots.append(
                 plotClasses.Plot(
                     ROOT.TH1F(histoname,histotitle,nbins,bins),
-                    discr,sel,catlabel))
+                    varname,sel,catlabel))
 
         elif "minxval" in subdict and "maxxval" in subdict:
             nbins = subdict["nhistobins"]
@@ -357,7 +357,7 @@ def init_plots(dictionary, data = None):
             plots.append(
                 plotClasses.Plot(
                     ROOT.TH1F(histoname, histotitle,nbins,xmin, xmax),
-                    discr,sel,catlabel))
+                    varname,sel,catlabel))
 
     if not data is None:
         data.categories.update(dictionary)
