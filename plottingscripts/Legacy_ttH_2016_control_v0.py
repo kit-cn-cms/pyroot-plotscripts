adf06fa4ca15a7515af7915745a786a82917437f
sebwieland <wielandseb@googlemail.com>
2019-11-11 12:20:00 +0100

    added controlplot configs for all years

--- plottingscripts/Legacy_ttH_2016_control_v0.py
+++ plottingscripts/Legacy_ttH_2016_control_v0.py
@@ -29,7 +29,7 @@ def main(pyrootdir, opts):
     # ========================================================
     '''
     # name of the analysis (i.e. workdir name)
-    name = 'ttHControlPlots_16_'
+    name = 'ttHControlPlots_2016'
 
     # path to workdir subfolder where all information should be saved
     workdir = pyrootdir + "/workdir/" + name
@@ -48,10 +48,10 @@ def main(pyrootdir, opts):
     memexp = '(memDBp>=0.0)*(memDBp)+(memDBp<0.0)*(0.01)+(memDBp==1.0)*(0.01)'
 
     # configs
-    config          = "ttH17_legacy/pltcfg_2016_combined_tthf_4FS_synced_v2"
+    config          = "ttH17_legacy_v4/samples_2016_5FS_synced_v0"
     variable_cfg    = "ttH17_legacy/additionalVariables"
-    plot_cfg        = "ttH17_legacy/controlPlots_v2"
-    syst_cfg        = "ttH17_legacy/systs_mergedTTbb_combined_tthf_synced_v2"
+    plot_cfg        = "ttH17_legacy_v4/controlPlots_v2"
+    syst_cfg        = "ttH17_legacy_v4/systs_4FS_5FS_synced_v4"
 
     # file for rate factors
     rateFactorsFile = pyrootdir + "/data/rateFactors/rateFactors_2016.csv"
