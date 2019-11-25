31cd22c9dc161b0c576cd51351c755a268a5db17
Jan van der Linden <jan.linden@student.kit.edu>
2019-11-25 12:17:01 +0100

    no 4FS sample for 2016 yet

--- configs/legacyAnalysis/samples_2016_5FS.py
+++ configs/legacyAnalysis/samples_2016_5FS.py
@@ -352,7 +352,7 @@ samples = [
     ]
 
 samples += samples_tH
-samples += samples_ttbb_4FS
+#samples += samples_ttbb_4FS
 samples += samples_minor_backgrounds
 #samples += samples_ttbar_hf_spilt
 #samples += samples_ttH_decay
