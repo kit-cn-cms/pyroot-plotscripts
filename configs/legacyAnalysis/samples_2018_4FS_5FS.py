227550df8e851ec1f29773a75951bdc005d4f020
sebwieland <wielandseb@googlemail.com>
2019-12-19 12:30:35 +0100

    somehow even selection for ttbb got lost

--- configs/legacyAnalysis/samples_2018_4FS_5FS.py
+++ configs/legacyAnalysis/samples_2018_4FS_5FS.py
@@ -332,7 +332,7 @@ samples_tH = [
 samples_ttbb_4FS = [
      plotClasses.Sample('t#bar{t}+b#bar{b} (4FS)',ROOT.kRed+3,
              path_ttbb,
-             lumi+TTbbSLDLweight+ttbb_4FS_scale+'*((GenEvt_I_TTPlusBB==1)||(GenEvt_I_TTPlusBB==2)||(GenEvt_I_TTPlusBB==3))'+sel_MET+sel_StrangeMuWeights,
+             lumi+TTbbSLDLweight+evenSel+ttbb_4FS_scale+'*((GenEvt_I_TTPlusBB==1)||(GenEvt_I_TTPlusBB==2)||(GenEvt_I_TTPlusBB==3))'+sel_MET+sel_StrangeMuWeights,
              'ttbb',
              samDict=sampleDict, readTrees=doReadTrees),
 
