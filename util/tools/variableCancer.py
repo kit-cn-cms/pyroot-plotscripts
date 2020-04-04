b43cd0512541e58bffbe532c125a31399bc5d79d
Michael Wassmer <michael.wassmer@cern.ch>
2020-04-04 18:03:26 +0200

    reserve 40 entries for array variables

--- util/tools/variableCancer.py
+++ util/tools/variableCancer.py
@@ -564,14 +564,14 @@ class Variable:
         # manage array variables
         if self.isArray:
             if varType == "F":
-                text = "    std::unique_ptr<float[]> "+varName+" ( new float[20] );\n"
-                text+= "    std::fill_n ("+varName+".get(), 20, -999);\n"
+                text = "    std::unique_ptr<float[]> "+varName+" ( new float[40] );\n"
+                text+= "    std::fill_n ("+varName+".get(), 40, -999);\n"
             elif varType == "I":
-                text = "    std::unique_ptr<Long64_t[]> "+varName+" ( new Long64_t[20] );\n"
-                text+= "    std::fill_n ("+varName+".get(), 20, -999);\n"
+                text = "    std::unique_ptr<Int_t[]> "+varName+" ( new Int_t[40] );\n"
+                text+= "    std::fill_n ("+varName+".get(), 40, -999);\n"
             elif varType == "L":
-                text = "    std::unique_ptr<Long64_t[]> "+varName+" ( new Long64_t[20] );\n"
-                text+= "    std::fill_n ("+varName+".get(), 20, -999);\n"
+                text = "    std::unique_ptr<Long64_t[]> "+varName+" ( new Long64_t[40] );\n"
+                text+= "    std::fill_n ("+varName+".get(), 40, -999);\n"
             else: print("UNKNOWN TYPE: "+str(varType))
 
         else:
