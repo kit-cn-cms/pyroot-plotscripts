5667ec7ebee6fb7704f80f60f7cfa55a89e8a186
sebwieland <wielandseb@googlemail.com>
2020-05-06 08:45:24 +0200

    updates with new DNN

---
+++ configs/legacyAnalysis/DNN_06-05-2020/DNNInputData/final_acceptance_ge4j_ge4t/command.sh
@@ -0,0 +1,1 @@
+python /portal/ekpbms1/home/swieland/ttH/DRACO-MLfoy/train_scripts/train_template.py -o legacy_16_17_18/final_acceptance_test -i /local/scratch/ssd/swieland/h5Files/SL_16_17_18_acceptance -a ttHbb,ttHnonbb,ttmb,ttlf,ttcc,tHq,tHW,tt2b -v variables_ttHbb_SL_final -e 1000 -s -1 -RGp -S ttHbb,ttHnonbb -n ttH_SL_legacy -c ge4j_ge4t
