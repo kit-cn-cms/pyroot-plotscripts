fd4f14c0951329a5c8b3bf3b1b822212e97b666f
sebwieland <wielandseb@googlemail.com>
2020-06-10 15:56:05 +0200

    added finalDNNs

---
+++ configs/legacyAnalysis/finalDNN/DNNInputData/ge4j_ge4t/STXS/command.sh
@@ -0,0 +1,1 @@
+python /portal/ekpbms1/home/swieland/ttH/DRACO-MLfoy/train_scripts/train_template.py -o legacy_16_17_18/final_STXS_acceptance_optimized_Taylor -i /local/scratch/ssd/swieland/h5Files/SL_16_17_18_acceptance -a ttHbb_STXS_0,ttHbb_STXS_1,ttHbb_STXS_2,ttHbb_STXS_3,ttHbb_STXS_4, -v variables_ttHbb_SL_STXS_final -e 1000 -S ttHbb_STXS_0 -s -1 -RpG -n ttH_SL_legacy_STXS_opt44 -c ge4j_ge4t
