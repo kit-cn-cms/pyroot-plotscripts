1c2b74b06aa1874ef36d0e010ff534c040c95659
Jan van der Linden <jan.linden@student.kit.edu>
2020-07-14 08:35:55 +0200

    cleanup

--- configs/legacyAnalysis/DNN_16-03-2020/DNNInputData/v1_top10/ge4j_3t/command.sh
+++
@@ -1,1 +0,0 @@
-python /portal/ekpbms3/home/vanderlinden/MLfoy/train_scripts/train_template.py -i /storage/8/vanderlinden/MLfoyData/legacyTTH/v1 -o legacyTTH/v1_top10 -a ttH,tHq,tHW,ttmb,tt2b,ttcc,ttlf -v variables_ttHbb_SL_inputValidation_2018_v2 -n ttH_SL_legacy -e 1000 -p -s -1 -S ttH -RV -c ge4j_3t
