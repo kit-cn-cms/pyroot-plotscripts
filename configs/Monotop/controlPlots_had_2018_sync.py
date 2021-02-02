e3417c08132dc1d48138b409073b941d92cc1092
Michael Wassmer <michael.wassmer@cern.ch>
2021-02-02 10:09:58 +0100

    edit deepak uncertainties to consider pass and fail tagging regions

--- configs/Monotop/controlPlots_had_2018_sync.py
+++ configs/Monotop/controlPlots_had_2018_sync.py
@@ -1288,7 +1288,7 @@ generalselection += "*(AK15Jet_CHF[0]>0.1)*(AK15Jet_NHF[0]<0.8)"
 generalselection += "*((AK15Jet_PuppiSoftDropMass[0]*weight_sdm_corr)>105.)*((AK15Jet_PuppiSoftDropMass[0]*weight_sdm_corr)<210.)"
 
 # top-tagger cut
-generalselection += "*(AK15Jet_DeepAK15_TvsQCD[0]>0.3)"
+generalselection += "*(AK15Jet_DeepAK15_TvsQCD[0]>=0.3)"
 
 def control_plots_had_SR(data=None):
     label = "#scale[0.8]{signal region (hadronic)}"
