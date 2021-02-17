0c12920891f5ba45f889645921bf13653b5ef8c1
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-02-17 21:05:49 +0100

    DNN sets without MEM

---
+++ configs/legacyAnalysis/noMEM_sets/kNN_2D_set/DNNInputData/ge4j_3t/1_classification/command.sh
@@ -0,0 +1,1 @@
+python /ceph/pkeicher/ttH_legacy/DRACO-MLfoy/train_scripts/train_template.py -o legacy_16_17_18/noMEM_kNN_2D_vars -i /local/scratch/ssd/swieland/h5Files/SL_16_17_18 -a ttH,ttmb,ttlf,ttcc,tHq,tHW,tt2b -v variables_ttHbb_SL_final_new -e 1000 -s -1 -RpG -S ttH -n ttH_SL_legacy_opt43 -c ge4j_3t -f 0.1
