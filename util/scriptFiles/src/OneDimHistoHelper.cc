63fd535ab2ff82a29903e3a03b33dc8b44b94092
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-03-04 17:10:03 +0100

    removed print out, added comments

--- util/scriptFiles/src/OneDimHistoHelper.cc
+++ util/scriptFiles/src/OneDimHistoHelper.cc
@@ -1,20 +1,5 @@
 #include "../interface/OneDimHistoHelper.h"
 
-OneDimHistoHelper::OneDimHistoHelper(){
-    std::cout << "initialized empty OneDimHistoHelper!" << std::endl;
-}
-
-OneDimHistoHelper::OneDimHistoHelper(std::map<std::string, std::shared_ptr<TH1>> input_map)
-{
-  histo_map = input_map;
-}
-OneDimHistoHelper::OneDimHistoHelper(std::string& input_variation, bool& input_skipWeightSysts, 
-                    std::vector<std::string>& input_systematics)
-{
-  variation = input_variation;
-  skipWeightSysts = input_skipWeightSysts;
-  systematics = input_systematics;    
-}
 OneDimHistoHelper::~OneDimHistoHelper(){}
 
 
@@ -23,17 +8,20 @@ void OneDimHistoHelper::fillNecessaryHistograms (const std::string& plotname,
                               const std::map<std::string, float>& weight_expressions, 
                               const float& variable) const
 {
+  /*
+    Basic idea: first collect all relevant templates for plot category 'plotname'.
+    relevant templates are identified by cross referencing the list of all 
+    weight expressions 'weight_expressions' with the current list of
+    relevant systematics 'systematics'.
+
+    After the templates are collected, they are filled in the 'helperFillHisto' function
+  */
   std::vector<structHelpFillHisto> helpWeightVec;
-  std::cout << "current map: " << std::endl;
-  for(auto entry: histo_map){
-        std::cout << "\t" << entry.first << std::endl;
-    }
-  std::cout << "OneDimHistoHelper::fillNecessaryHistograms: fill '" << plotname+variation <<"'" << std::endl;
+  // the nominal template is always relevant, so add it to the list
   helpWeightVec.push_back({histo_map.at(plotname+variation).get(), ((NomWeight)*(final_weight))});
   if (!skipWeightSysts) { // append plots for weight systs if neccessary
     for(auto& systname : systematics){
       if (weight_expressions.find(systname) != weight_expressions.end()){
-          std::cout << "OneDimHistoHelper::fillNecessaryHistograms: fill '" << plotname+systname <<"'" << std::endl;
         helpWeightVec.push_back( { histo_map.at(plotname+systname).get(), 
                                     (weight_expressions.at(systname))*(final_weight) 
                                 } );
@@ -46,6 +34,10 @@ void OneDimHistoHelper::fillNecessaryHistograms (const std::string& plotname,
 // helper function to fill plots more efficiently
 void OneDimHistoHelper::helperFillHisto(const std::vector<structHelpFillHisto>& paramVec, const double& val) const
 {
+  /*
+        Fill templates. First check whether the variable(s) to be filled are not in the under/overflow bin of the template.
+        Then fill while considering the weight
+    */
   for (const auto &singleParams: paramVec)
   // singleParams: histo, var, weight
   {
@@ -53,8 +45,3 @@ void OneDimHistoHelper::helperFillHisto(const std::vector<structHelpFillHisto>& 
         singleParams.histo->Fill(fmin(singleParams.histo->GetXaxis()->GetXmax()-1e-6,fmax(singleParams.histo->GetXaxis()->GetXmin()+1e-6,val)),singleParams.weight);
   }
 }
-
-// void OneDimHistoHelper::push_back(std::string plotname, std::shared_ptr<TH1> histo){
-//     histo_map[plotname] = histo;
-//     histo_map[plotname]->SetDirectory(0);
-// }
