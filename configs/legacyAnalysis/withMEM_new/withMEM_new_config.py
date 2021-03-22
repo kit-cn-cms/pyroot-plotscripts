5464f00f43c6facd62f179042446b40e7d33261e
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-03-22 22:01:19 +0100

    bug fix: 3t DNNs still included observables that were discarded after 1D GoF

--- configs/legacyAnalysis/withMEM_new/withMEM_new_config.py
+++ configs/legacyAnalysis/withMEM_new/withMEM_new_config.py
@@ -81,15 +81,15 @@ def plots_dnn_ttH_vs_slike(data, discrname, category, selection, label):
     interfaces = []
 
 
-    interf_ttH_vs_ttB = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttH/(DNNOutput_{cat}_node_ttH + DNNOutput_{cat}_node_ttmb + DNNOutput_{cat}_node_tt2b)".format(cat = category),
+    interf_ttH_vs_slike_merged = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttH/(DNNOutput_{cat}_node_ttH + DNNOutput_{cat}_node_ttmb + DNNOutput_{cat}_node_tt2b)".format(cat = category),
                                             label          = "ljets_{}_ttH_vs_slike_merged".format(category),
                                             selection      = "({sel}&&(1.)&&(DNNPredictedClass_{cat}==0))".format(sel = selection, cat = category))
-    interf_ttH_vs_ttB.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==0 || DNNPredictedClass_{cat}==1 || DNNPredictedClass_{cat}==2))".format(cat=category, sel = selection),"ljets_{}_ttH_vs_slike_merged".format(category),"")
-    interf_ttH_vs_ttB.category_label = label
-    interf_ttH_vs_ttB.minxval = 0.2
-    interf_ttH_vs_ttB.maxxval = 1.0
-    interf_ttH_vs_ttB.nhistobins = ndefaultbins
-    interfaces.append(interf_ttH_vs_ttB)
+    interf_ttH_vs_slike_merged.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==0 || DNNPredictedClass_{cat}==1 || DNNPredictedClass_{cat}==2))".format(cat=category, sel = selection),"ljets_{}_ttH_vs_slike_merged".format(category),"")
+    interf_ttH_vs_slike_merged.category_label = label
+    interf_ttH_vs_slike_merged.minxval = 0.2
+    interf_ttH_vs_slike_merged.maxxval = 1.0
+    interf_ttH_vs_slike_merged.nhistobins = ndefaultbins
+    interfaces.append(interf_ttH_vs_slike_merged)
 
     interf_ttH_vs_slike = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttH/(DNNOutput_{cat}_node_ttH + DNNOutput_{cat}_node_ttmb + DNNOutput_{cat}_node_tt2b)".format(cat = category),
                                             label          = "ljets_{}_ttH_vs_slike".format(category),
@@ -154,9 +154,9 @@ def plots_dnn_X_vs_ttLF(data, discrname, category, selection, label):
     ndefaultbins = 50
     interfaces = []
     interf_ttcc_vs_ttLFlike = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttcc/(DNNOutput_{cat}_node_ttcc + DNNOutput_{cat}_node_ttlf)".format(cat = category),
-                                            label          = "ljets_{}_ttcc_vs_ttLFlike".format(category),
+                                            label          = "ljets_{}_ttcc_vs_ttLFlike_separate".format(category),
                                             selection      = "({sel}&&(1.)&&(DNNPredictedClass_{cat}==0))".format(sel = selection, cat = category))
-    interf_ttcc_vs_ttLFlike.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==3 || DNNPredictedClass_{cat}==4))".format(cat=category, sel = selection),"ljets_{}_ttcc_vs_ttLFlike".format(category),"")
+    interf_ttcc_vs_ttLFlike.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==3))".format(cat=category, sel = selection),"ljets_{}_ttcc_vs_ttLFlike_separate".format(category),"")
     interf_ttcc_vs_ttLFlike.category_label = label
     interf_ttcc_vs_ttLFlike.minxval = 0.2
     interf_ttcc_vs_ttLFlike.maxxval = 1.0
@@ -164,14 +164,34 @@ def plots_dnn_X_vs_ttLF(data, discrname, category, selection, label):
     interfaces.append(interf_ttcc_vs_ttLFlike)
 
     interf_ttlf_vs_ttLFlike = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttlf/(DNNOutput_{cat}_node_ttcc + DNNOutput_{cat}_node_ttlf)".format(cat = category),
-                                            label          = "ljets_{}_ttlf_vs_ttLFlike".format(category),
+                                            label          = "ljets_{}_ttlf_vs_ttLFlike_separate".format(category),
                                             selection      = "({sel}&&(1.)&&(DNNPredictedClass_{cat}==0))".format(sel = selection, cat = category))
-    interf_ttlf_vs_ttLFlike.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==3 || DNNPredictedClass_{cat}==4))".format(cat=category, sel = selection),"ljets_{}_ttlf_vs_ttLFlike".format(category),"")
+    interf_ttlf_vs_ttLFlike.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==4))".format(cat=category, sel = selection),"ljets_{}_ttlf_vs_ttLFlike_separate".format(category),"")
     interf_ttlf_vs_ttLFlike.category_label = label
     interf_ttlf_vs_ttLFlike.minxval = 0.2
     interf_ttlf_vs_ttLFlike.maxxval = 1.0
     interf_ttlf_vs_ttLFlike.nhistobins = ndefaultbins
     interfaces.append(interf_ttlf_vs_ttLFlike)
+
+    interf_ttlf_vs_ttLFlike_merged = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttlf/(DNNOutput_{cat}_node_ttcc + DNNOutput_{cat}_node_ttlf)".format(cat = category),
+                                            label          = "ljets_{}_ttlf_vs_ttLFlike".format(category),
+                                            selection      = "({sel}&&(1.)&&(DNNPredictedClass_{cat}==0))".format(sel = selection, cat = category))
+    interf_ttlf_vs_ttLFlike_merged.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==3 || DNNPredictedClass_{cat}==4))".format(cat=category, sel = selection),"ljets_{}_ttlf_vs_ttLFlike".format(category),"")
+    interf_ttlf_vs_ttLFlike_merged.category_label = label
+    interf_ttlf_vs_ttLFlike_merged.minxval = 0.2
+    interf_ttlf_vs_ttLFlike_merged.maxxval = 1.0
+    interf_ttlf_vs_ttLFlike_merged.nhistobins = ndefaultbins
+    interfaces.append(interf_ttlf_vs_ttLFlike_merged)
+
+    interf_ttcc_vs_ttLFlike_merged = vhi.variableHistoInterface(variable_name  = "DNNOutput_{cat}_node_ttcc/(DNNOutput_{cat}_node_ttcc + DNNOutput_{cat}_node_ttlf)".format(cat = category),
+                                            label          = "ljets_{}_ttcc_vs_ttLFlike".format(category),
+                                            selection      = "({sel}&&(1.)&&(DNNPredictedClass_{cat}==0))".format(sel = selection, cat = category))
+    interf_ttcc_vs_ttLFlike_merged.category = ("({sel}&&(1.)&&(DNNPredictedClass_{cat}==3 || DNNPredictedClass_{cat}==4))".format(cat=category, sel = selection),"ljets_{}_ttcc_vs_ttLFlike".format(category),"")
+    interf_ttcc_vs_ttLFlike_merged.category_label = label
+    interf_ttcc_vs_ttLFlike_merged.minxval = 0.2
+    interf_ttcc_vs_ttLFlike_merged.maxxval = 1.0
+    interf_ttcc_vs_ttLFlike_merged.nhistobins = ndefaultbins
+    interfaces.append(interf_ttcc_vs_ttLFlike_merged)
 
     for interf in interfaces:
         l = interf.label
@@ -406,7 +426,7 @@ def plots_dnn(data, discrname):
     interf_ljets_ge4j_ge4t_optimized_tHW_node.nhistobins = ndefaultbins
     interfaces.append(interf_ljets_ge4j_ge4t_optimized_tHW_node)
 
-    plots for ge4j_3t_classification
+    # plots for ge4j_3t_classification
 
     interf_ljets_ge4j_3t_classification_ttH_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_classification_node_ttH",
                                             label          = "ljets_ge4j_3t_classification_ttH_node",
@@ -478,6 +498,78 @@ def plots_dnn(data, discrname):
     interf_ljets_ge4j_3t_classification_tHW_node.nhistobins = ndefaultbins
     interfaces.append(interf_ljets_ge4j_3t_classification_tHW_node)
     
+    # plots for ge4j_3t_noMEM_pruned_set_v2
+
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_ttH",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==0))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==0))","ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node.maxxval = 0.58
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node.nhistobins = ndefaultbins
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttH_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_ttmb",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==1))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==1))","ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node.maxxval = 0.45
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node.nhistobins = ndefaultbins
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttmb_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_tt2b",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==2))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==2))","ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node.maxxval = 0.58
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node.nhistobins = 1
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tt2b_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_ttcc",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==3))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==3))","ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node.maxxval = 0.42
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node.nhistobins = 1
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttcc_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_ttlf",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==4))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==4))","ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node.maxxval = 0.64
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node.nhistobins = 1
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_ttlf_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_tHq",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==5))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==5))","ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node.maxxval = 0.95
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node.nhistobins = ndefaultbins
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHq_node)
+    
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node = vhi.variableHistoInterface(variable_name  = "DNNOutput_ge4j_3t_noMEM_pruned_set_v2_node_tHW",
+                                            label          = "ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node",
+                                            selection      = "((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==6))")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node.category = ("((N_Jets>=4&&N_BTagsM==3)&&(1.)&&(DNNPredictedClass_ge4j_3t_noMEM_pruned_set_v2==6))","ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node","")
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node.category_label = "\geq 4 jets, 3 b-tags"
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node.minxval = 0.14
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node.maxxval = 0.99
+    interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node.nhistobins = ndefaultbins
+    interfaces.append(interf_ljets_ge4j_3t_noMEM_pruned_set_v2_tHW_node)
+    
 
     for interf in interfaces:
         l = interf.label
@@ -497,6 +589,9 @@ def getDiscriminatorPlots(data = None, discrname = ''):
     selection = "(N_Jets>=4&&N_BTagsM==3)"
     discriminatorPlots += plots_dnn_ttH_vs_slike(data = data, discrname=discrname, category="ge4j_3t_classification", selection= selection, label = label )
     discriminatorPlots += plots_dnn_X_vs_ttLF(data = data, discrname=discrname, category="ge4j_3t_classification", selection= selection, label = label )
+
+    discriminatorPlots += plots_dnn_ttH_vs_slike(data = data, discrname=discrname, category="ge4j_3t_noMEM_pruned_set_v2", selection= selection, label = label )
+    discriminatorPlots += plots_dnn_X_vs_ttLF(data = data, discrname=discrname, category="ge4j_3t_noMEM_pruned_set_v2", selection= selection, label = label )
 
     label = "\geq 4 jets, \geq 4 b-tags"
     selection = "(N_Jets>=4&&N_BTagsM>=4)"
