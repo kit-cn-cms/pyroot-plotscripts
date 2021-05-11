3a28d62d43e2139ac9cd8190e59badcdb7eb8200
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-05-11 13:23:12 +0200

    config to do cross check for ttH CP sample

--- configs/legacyAnalysis/withMEM_new/withMEM_final_config_CPscan_ttH.py
+++ configs/legacyAnalysis/withMEM_new/withMEM_final_config_CPscan_ttH.py
@@ -23,27 +23,27 @@ def plots_ge4j_ge4t_classification(data = None, label = "\geq 4 jets, \geq 4 b-t
     interfaces = []
 
     plots = [
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_CSV_2".format(name),"CSV[2]",50,0.277,1.0),"CSV[2]",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_CSV_avg".format(name),"Evt_CSV_avg",50,0.15,1.0),"Evt_CSV_avg",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_CSV_avg_tagged".format(name),"Evt_CSV_avg_tagged",50,0.3,1.0),"Evt_CSV_avg_tagged",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Deta_JetsAverage".format(name),"Evt_Deta_JetsAverage",50,0.0,3.0),"Evt_Deta_JetsAverage",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Deta_TaggedJetsAverage".format(name),"Evt_Deta_TaggedJetsAverage",50,0.0,3.0),"Evt_Deta_TaggedJetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_CSV_2".format(name),"CSV[2]",50,0.277,1.0),"CSV[2]",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_CSV_avg".format(name),"Evt_CSV_avg",50,0.15,1.0),"Evt_CSV_avg",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_CSV_avg_tagged".format(name),"Evt_CSV_avg_tagged",50,0.3,1.0),"Evt_CSV_avg_tagged",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Deta_JetsAverage".format(name),"Evt_Deta_JetsAverage",50,0.0,3.0),"Evt_Deta_JetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Deta_TaggedJetsAverage".format(name),"Evt_Deta_TaggedJetsAverage",50,0.0,3.0),"Evt_Deta_TaggedJetsAverage",selection,label),
         plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_HT_tags".format(name),"Evt_HT_tags",50,100.0,1000.0),"Evt_HT_tags",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_M2_minDrTaggedJets".format(name),"M_{2}( min #DeltaR(tag,tag) )",50,0.0,400.0),"Evt_M2_minDrTaggedJets",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_M_TaggedJetsAverage".format(name),"Evt_M_TaggedJetsAverage",50,5.0,70.0),"Evt_M_TaggedJetsAverage",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_JetsAverage".format(name),"Evt_Pt_JetsAverage",50,30.0,500.0),"Evt_Pt_JetsAverage",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_TaggedJetsAverage".format(name),"Evt_Pt_TaggedJetsAverage",50,30.0,600.0),"Evt_Pt_TaggedJetsAverage",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_minDrTaggedJets".format(name),"Evt_Pt_minDrTaggedJets",50,20.0,600.0),"Evt_Pt_minDrTaggedJets",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_blr_transformed".format(name),"Evt_blr_transformed",50,-2.5,15.0),"Evt_blr_transformed",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_M2_minDrTaggedJets".format(name),"M_{2}( min #DeltaR(tag,tag) )",50,0.0,400.0),"Evt_M2_minDrTaggedJets",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_M_TaggedJetsAverage".format(name),"Evt_M_TaggedJetsAverage",50,5.0,70.0),"Evt_M_TaggedJetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_JetsAverage".format(name),"Evt_Pt_JetsAverage",50,30.0,500.0),"Evt_Pt_JetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_TaggedJetsAverage".format(name),"Evt_Pt_TaggedJetsAverage",50,30.0,600.0),"Evt_Pt_TaggedJetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_Pt_minDrTaggedJets".format(name),"Evt_Pt_minDrTaggedJets",50,20.0,600.0),"Evt_Pt_minDrTaggedJets",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Evt_blr_transformed".format(name),"Evt_blr_transformed",50,-2.5,15.0),"Evt_blr_transformed",selection,label),
         plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_N_Jets".format(name),"N_Jets",7,3.5,10.5),"N_Jets",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHW_Jet_CSV_btop".format(name),"Reco_JABDT_tHW_Jet_CSV_btop",50,-1.5,1.0),"Reco_JABDT_tHW_Jet_CSV_btop",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHW_top_pt__P__h_pt__P__wb_pt__DIV__Evt_HT__P__Evt_Pt_MET__P__Lep_Pt".format(name),"Reco_JABDT_tHW_energy_fraction",50,-1.5,1.0),"Reco_JABDT_tHW_top_pt__P__h_pt__P__wb_pt__DIV__Evt_HT__P__Evt_Pt_MET__P__Lep_Pt",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHq_abs_ljet_eta".format(name),"Reco_JABDT_tHq_abs_ljet_eta",50,-1.5,4.5),"Reco_JABDT_tHq_abs_ljet_eta",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_tHq_bestJABDToutput".format(name),"Reco_tHq_bestJABDToutput",50,-1.0,0.7),"Reco_tHq_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttH_bestJABDToutput".format(name),"Reco_ttH_bestJABDToutput",50,-1.0,0.7),"Reco_ttH_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttH_toplep_m".format(name),"Reco_ttH_toplep_m",50,-1.5,750.0),"Reco_ttH_toplep_m",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttbar_bestJABDToutput".format(name),"Reco_ttbar_bestJABDToutput",50,-0.4,0.7),"Reco_ttbar_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_memDBp".format(name),"MEM",50,-2,1.0),memexp,selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHW_Jet_CSV_btop".format(name),"Reco_JABDT_tHW_Jet_CSV_btop",50,-1.5,1.0),"Reco_JABDT_tHW_Jet_CSV_btop",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHW_top_pt__P__h_pt__P__wb_pt__DIV__Evt_HT__P__Evt_Pt_MET__P__Lep_Pt".format(name),"Reco_JABDT_tHW_energy_fraction",50,-1.5,1.0),"Reco_JABDT_tHW_top_pt__P__h_pt__P__wb_pt__DIV__Evt_HT__P__Evt_Pt_MET__P__Lep_Pt",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_JABDT_tHq_abs_ljet_eta".format(name),"Reco_JABDT_tHq_abs_ljet_eta",50,-1.5,4.5),"Reco_JABDT_tHq_abs_ljet_eta",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_tHq_bestJABDToutput".format(name),"Reco_tHq_bestJABDToutput",50,-1.0,0.7),"Reco_tHq_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttH_bestJABDToutput".format(name),"Reco_ttH_bestJABDToutput",50,-1.0,0.7),"Reco_ttH_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttH_toplep_m".format(name),"Reco_ttH_toplep_m",50,-1.5,750.0),"Reco_ttH_toplep_m",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_Reco_ttbar_bestJABDToutput".format(name),"Reco_ttbar_bestJABDToutput",50,-0.4,0.7),"Reco_ttbar_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_ge4t_{}_memDBp".format(name),"MEM",50,-2,1.0),memexp,selection,label),
         ]
 
         
@@ -55,17 +55,20 @@ def plots_ge4j_3t_classification(data = None, label = "\geq 4 jets, 3 b-tags", \
                                 selection = "(N_Jets>=4&&N_BTagsM==3)&&(1.)", name = "classification"): 
     interfaces = []
     plots = [
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_CSV_avg","Evt_CSV_avg".format(name),50,0.15,1.0),"Evt_CSV_avg",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_CSV_dev".format(name),"Evt_CSV_dev",50,0.0,0.25),"Evt_CSV_dev",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_HT_jets".format(name),"H_{T}(jets)",50,200.0,1500.0),"Evt_HT_jets",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_M_JetsAverage".format(name),"Evt_M_JetsAverage",50,5.0,50.0),"Evt_M_JetsAverage",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_M_Total".format(name),"Evt_M_Total",50,200.0,3000.0),"Evt_M_Total",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Jet_Pt_0".format(name),"p_{T} of leading jet",50,0.0,600.0),"Jet_Pt[0]",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_JABDT_tHW_Jet_CSV_btop".format(name),"Reco_JABDT_tHW_Jet_CSV_btop",50,-1.5,1.0),"Reco_JABDT_tHW_Jet_CSV_btop",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_tHq_bestJABDToutput".format(name),"Reco_tHq_bestJABDToutput",50,-1.0,0.7),"Reco_tHq_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_ttH_bestJABDToutput".format(name),"Reco_ttH_bestJABDToutput",50,-1.0,0.7),"Reco_ttH_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_ttbar_bestJABDToutput".format(name),"Reco_ttbar_bestJABDToutput",50,-0.4,0.7),"Reco_ttbar_bestJABDToutput",selection,label),
-        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_memDBp".format(name),"MEM",50,-2.,1.0),memexp,selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_CSV_avg","Evt_CSV_avg".format(name),50,0.15,1.0),"Evt_CSV_avg",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_CSV_dev".format(name),"Evt_CSV_dev",50,0.0,0.25),"Evt_CSV_dev",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_HT_jets".format(name),"H_{T}(jets)",50,200.0,1500.0),"Evt_HT_jets",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_M_JetsAverage".format(name),"Evt_M_JetsAverage",50,5.0,50.0),"Evt_M_JetsAverage",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_M_Total".format(name),"Evt_M_Total",50,200.0,3000.0),"Evt_M_Total",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Jet_Pt_0".format(name),"p_{T} of leading jet",50,0.0,600.0),"Jet_Pt[0]",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_JABDT_tHW_Jet_CSV_btop".format(name),"Reco_JABDT_tHW_Jet_CSV_btop",50,-1.5,1.0),"Reco_JABDT_tHW_Jet_CSV_btop",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_tHq_bestJABDToutput".format(name),"Reco_tHq_bestJABDToutput",50,-1.0,0.7),"Reco_tHq_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_ttH_bestJABDToutput".format(name),"Reco_ttH_bestJABDToutput",50,-1.0,0.7),"Reco_ttH_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Reco_ttbar_bestJABDToutput".format(name),"Reco_ttbar_bestJABDToutput",50,-0.4,0.7),"Reco_ttbar_bestJABDToutput",selection,label),
+        # plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_memDBp".format(name),"MEM",50,-2.,1.0),memexp,selection,label),
+        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_N_Jets".format(name),"N_Jets",7,3.5,10.5),"N_Jets",selection,label),
+        plotClasses.Plot(ROOT.TH1D("ljets_ge4j_3t_{}_Evt_HT_tags".format(name),"Evt_HT_tags",50,100.0,1000.0),"Evt_HT_tags",selection,label),
+
         ]
 
         
@@ -257,16 +260,16 @@ def plots_dnn(data, discrname):
 
 def getDiscriminatorPlots(data = None, discrname = ''):
     discriminatorPlots = []
-    # discriminatorPlots += plots_ge4j_ge4t_classification(data)
-    # discriminatorPlots += plots_ge4j_3t_classification(data)
+    discriminatorPlots += plots_ge4j_ge4t_classification(data)
+    discriminatorPlots += plots_ge4j_3t_classification(data)
     label = "\geq 4 jets, 3 b-tags"
     selection = "(N_Jets>=4&&N_BTagsM==3)"
  
     label = "\geq 4 jets, \geq 4 b-tags"
     selection = "(N_Jets>=4&&N_BTagsM>=4)"
-    discriminatorPlots += plots_dnn_ttH_vs_slike(data = data, discrname=discrname, category="ge4j_ge4t_classification", selection= selection, label = label )
+    # discriminatorPlots += plots_dnn_ttH_vs_slike(data = data, discrname=discrname, category="ge4j_ge4t_classification", selection= selection, label = label )
     
-    discriminatorPlots += plots_dnn(data, discrname)
+    # discriminatorPlots += plots_dnn(data, discrname)
     return discriminatorPlots
 
 
