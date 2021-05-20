4f69851b816cc230711cd9a8bb1f06f1952123dd
Ellen Sarauer <ellen.sarauer@cern.ch>
2021-05-20 22:32:54 +0200

    copied all dnns for ttHH and adjusted netconfigs

--- configs/legacyAnalysis_ttHH/finalDNN/DNNInputData/copyscript_lowNJet_both.sh
+++ configs/legacyAnalysis_ttHH/finalDNN/DNNInputData/copyscript_lowNJet_both.sh
@@ -1,40 +1,47 @@
-mkdir low_NJet_both
-cd low_NJet_both
+mkdir low_NJetNTag
+cd low_NJetNTag
 mkdir le5j_le3t  le5j_le4t le6j_le3t  le6j_le4t le7j_le3t  le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le5j_le3t/checkpoints/* low_NJet_both/le5j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le5j_le4t/checkpoints/* low_NJet_both/le5j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le6j_le3t/checkpoints/* low_NJet_both/le6j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le6j_le4t/checkpoints/* low_NJet_both/le6j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le7j_le3t/checkpoints/* low_NJet_both/le7j_le3t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-scp -r esarauer@lx92.etp.kit.edu:/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le7j_le4t/checkpoints/* low_NJet_both/le7j_le4t
-#ge6j_le3t  ge6j_le4t    ge7j_le3t  ge7j_le4t    ge8j_le3t  ge8j_le4t
+mkdir le5j_le3t/DNN_30GeV_le5j_le3t le5j_le3t/DNN_60GeV_le5j_le3t le5j_le3t/DNN_90GeV_le5j_le3t le5j_le3t/DNN_withTTZZ_30GeV_le5j_le3t le5j_le3t/DNN_withTTZZ_60GeV_le5j_le3t le5j_le3t/DNN_withTTZZ_90GeV_le5j_le3t
+mkdir le5j_le4t/DNN_30GeV_le5j_le4t le5j_le4t/DNN_60GeV_le5j_le4t le5j_le4t/DNN_90GeV_le5j_le4t le5j_le4t/DNN_withTTZZ_30GeV_le5j_le4t le5j_le4t/DNN_withTTZZ_60GeV_le5j_le4t le5j_le4t/DNN_withTTZZ_90GeV_le5j_le4t
+mkdir le6j_le3t/DNN_30GeV_le6j_le3t le6j_le3t/DNN_60GeV_le6j_le3t le6j_le3t/DNN_90GeV_le6j_le3t le6j_le3t/DNN_withTTZZ_30GeV_le6j_le3t le6j_le3t/DNN_withTTZZ_60GeV_le6j_le3t le6j_le3t/DNN_withTTZZ_90GeV_le6j_le3t
+mkdir le6j_le4t/DNN_30GeV_le6j_le4t le6j_le4t/DNN_60GeV_le6j_le4t le6j_le4t/DNN_90GeV_le6j_le4t le6j_le4t/DNN_withTTZZ_30GeV_le6j_le4t le6j_le4t/DNN_withTTZZ_60GeV_le6j_le4t le6j_le4t/DNN_withTTZZ_90GeV_le6j_le4t
+mkdir le7j_le3t/DNN_30GeV_le7j_le3t le7j_le3t/DNN_60GeV_le7j_le3t le7j_le3t/DNN_90GeV_le7j_le3t le7j_le3t/DNN_withTTZZ_30GeV_le7j_le3t le7j_le3t/DNN_withTTZZ_60GeV_le7j_le3t le7j_le3t/DNN_withTTZZ_90GeV_le7j_le3t
+mkdir le7j_le4t/DNN_30GeV_le7j_le4t le7j_le4t/DNN_60GeV_le7j_le4t le7j_le4t/DNN_90GeV_le7j_le4t le7j_le4t/DNN_withTTZZ_30GeV_le7j_le4t le7j_le4t/DNN_withTTZZ_60GeV_le7j_le4t le7j_le4t/DNN_withTTZZ_90GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_30GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_60GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_90GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_withTTZZ_30GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_withTTZZ_60GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le5j_le3t/checkpoints/ le5j_le3t/DNN_withTTZZ_90GeV_le5j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_30GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_60GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_90GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_withTTZZ_30GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_withTTZZ_60GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le5j_le4t/checkpoints/ le5j_le4t/DNN_withTTZZ_90GeV_le5j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_30GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_60GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_90GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_withTTZZ_30GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_withTTZZ_60GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le6j_le3t/checkpoints/ le6j_le3t/DNN_withTTZZ_90GeV_le6j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_30GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_60GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_90GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_withTTZZ_30GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_withTTZZ_60GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le6j_le4t/checkpoints/ le6j_le4t/DNN_withTTZZ_90GeV_le6j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_30GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_60GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_90GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_withTTZZ_30GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_withTTZZ_60GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le7j_le3t/checkpoints/ le7j_le3t/DNN_withTTZZ_90GeV_le7j_le3t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_30GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_30GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_60GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_60GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_90GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_90GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_30GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_withTTZZ_30GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_60GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_withTTZZ_60GeV_le7j_le4t
+scp -r esarauer@lx92.etp.kit.edu:~/DracoMLfoy/DRACO-MLfoy/workdir/full_setup/low_NJet_both/DNN_withTTZZ_90GeV_le7j_le4t/checkpoints/ le7j_le4t/DNN_withTTZZ_90GeV_le7j_le4t
+#  le5j_le3t  le5j_le4t  le6j_le3t  le6j_le4t  le7j_le3t  le7j_le4t
+
