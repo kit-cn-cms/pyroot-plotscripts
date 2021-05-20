433749fe94c60696897c477dbceaa30c21dcc742
Ellen Sarauer <ellen.sarauer@cern.ch>
2021-05-20 22:35:37 +0200

    copied all dnns for ttHH and adjusted netconfigs

---
+++ configs/legacyAnalysis_ttHH/finalDNN/DNNInputData/low_NTag/ge8j_le3t/DNN_withTTZZ_60GeV_ge8j_le3t/command.sh
@@ -0,0 +1,1 @@
+python /home/esarauer/DracoMLfoy/DRACO-MLfoy/train_scripts/train_template.py -i /local/scratch/ssd/esarauer/final_ttHH -v variables_full_ttHH -n ttHH_2017 -S ttHH --plot -c ge8j_le3t -e 1000 --printroc -o full_setup/low_NJet_tags/DNN_withTTZZ_60GeV -f 0.1
