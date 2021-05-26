2194dd812edb063b5987bee792eebf6c19d26345
Ellen Sarauer <ellen.sarauer@cern.ch>
2021-05-26 16:27:39 +0200

    adjusted samples

--- plottingscripts/finalDNN_2017.py
+++ plottingscripts/finalDNN_2017.py
@@ -30,7 +30,7 @@ def main(pyrootdir, opts):
     ''')
     # name of the analysis (i.e. workdir name)
 
-    name = 'test/nanoAOD_automated_DNN_low_NJetNTag'
+    name = 'test/nanoAOD_DNN_low_NJetNTag_optimizedbinning'
 
     # path to workdir subfolder where all information should be saved
     workdir = pyrootdir + "/workdir/" + name
@@ -53,10 +53,10 @@ def main(pyrootdir, opts):
     # configs
     config          = "legacyAnalysis_ttHH/samples_2017"
     variable_cfg    = "legacyAnalysis_ttHH/additionalVariables"
-    plot_cfg        = "legacyAnalysis_ttHH/finalDNN/low_NJetNTag_DNN_config.py"
+    plot_cfg        = "legacyAnalysis_ttHH/finalDNN/low_NTag_DNN_config_optimized_binning.py"
     # plot_cfg        = "legacyAnalysis_ttHH/DNN_14-05-2020/combined161718_DNNs"
-    #syst_cfg        = "legacyAnalysis_ttHH/systs_2017_test"
-    syst_cfg        = "legacyAnalysis_ttHH/no_systs"
+    syst_cfg        = "legacyAnalysis_ttHH/systs_2017_test"
+    #syst_cfg        = "legacyAnalysis_ttHH/no_systs"
     replace_cfg     = ""
 
     #sfCorrection = {}
@@ -111,7 +111,7 @@ def main(pyrootdir, opts):
     # plotDataBases = [["memDB","/nfs/dust/cms/user/vdlinden/legacyTTH/memes/memTrees/2017/",True]] 
     # memDataBase = "/nfs/dust/cms/user/swieland/ttH_legacy/MEMdatabase/CodeforScriptGenerator/MEMDataBase/MEMDataBase"
     dnnInterface = {"interfacePath":    pyrootdir+"/util/dNNInterfaces/MLfoyInterface.py",
-                   "checkpointFiles":  pyrootdir+"/configs/legacyAnalysis_ttHH/finalDNN/DNNInputData/low_NJetNTag"}
+                   "checkpointFiles":  pyrootdir+"/configs/legacyAnalysis_ttHH/finalDNN/DNNInputData/low_NTag"}
     #dnnInterface = None
 
     # path to datacardMaker directory
